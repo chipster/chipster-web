8786ff72c66fd1e3ba25c435837022b9f8ac2e6a
Rumana Quazi <rquazi@gmail.com>
2015-05-19 14:53:59 +0300

    added ng-routing to app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
-angular.module('ChipsterWeb').config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/dataset',  {templateUrl: 'views/dataset.html'})
-                  .when('/profile',  {templateUrl: 'views/profile.html'});
+var chipsterWeb=angular.module('ChipsterWeb',['ngRoute']);
+
+//configure our route
+chipsterWeb.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider
+
+		//route for home page
+		.when('/',{
+			templateUrl:'views/home.html',
+			controller:'mainController'
+		})
+		//route for login page
+		.when('/login',{
+				templateUrl:'views/login.html',
+				controller:'loginController'
+		});
 }]);
+
+//main controller
+chipsterWeb.controller('mainController',function($scope){
+	$scope.message="it is working";
+});
+
+//login controller
+chipsterWeb.controller('loginController',function($scope){
+	$scope.message="This is login screen";
+});
+
+chipsterWeb.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+});
 
 
 
