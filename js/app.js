2eeb749eb9f7d92f7e0c04b1a1fcf7110c6fdee3
Rumana Quazi <rquazi@gmail.com>
2015-05-20 15:54:37 +0300

    added UI Bootstrap and fixed the tabset

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-var chipsterWeb=angular.module('ChipsterWeb',['ngRoute']);
-
+var chipsterWeb=angular.module('ChipsterWeb',['ngRoute','ui.bootstrap']);
 //configure our route
 chipsterWeb.config(['$routeProvider',
 	function($routeProvider){
@@ -14,6 +13,15 @@ chipsterWeb.config(['$routeProvider',
 		.when('/login',{
 				templateUrl:'views/login.html',
 				controller:'loginController'
+		})
+		.when('/dataset',{
+			templateUrl:'views/dataset.html'
+		})
+		.when('/analysisTools',{
+			templateUrl:'views/toolSource.html'
+		})
+		.when('/visualization',{
+			templateUrl:'views/visualization.html'
 		});
 }]);
 
@@ -29,6 +37,37 @@ chipsterWeb.controller('loginController',function($scope){
 
 chipsterWeb.controller('contactController', function($scope) {
 		$scope.message = 'Contact us! JK. This is just a demo.';
+});
+
+//Controller for different toolse
+chipsterWeb.controller('toolTabController', function($scope,$window){
+	$scope.tabs=[
+		{ title: 'Microarray', content:'toolset'},
+		{ title: 'NGS', content:'NGS toolset'},
+		{ title: 'Miscelleneous', content:'Misc content'}
+	];
+});
+
+//Controller for specific processing tools
+chipsterWeb.controller('toolAccordionController', function($scope){
+	$scope.oneAtATime=true;
+
+	$scope.groups=[
+		{
+			title: 'Normalization',
+			content: 'Simple Normalization'
+		},
+		{
+			title: 'Quality Control',
+			content: 'PCA Quality Control'
+		}
+	];
+
+	$scope.status={
+		isFirstOpen:true,
+		isFirstDisabled:false
+	};
+
 });
 
 
