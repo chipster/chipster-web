5750774828f1a1e6f7477f8094d4dea0c5e1e851
Rumana Quazi <rquazi@gmail.com>
2015-07-14 12:44:22 +0300

    population graph with NGS workflow data

--- js/controllers/WorkflowGraphCtrl.js
+++ js/controllers/WorkflowGraphCtrl.js
@@ -1,31 +1,12 @@
-chipsterWeb.controller('WorkflowGraphCtrl', ['$scope', function($scope){
-      
-      $scope.showObj = function(item){
-        $scope.item = item;
-        console.log($scope.item);
-      }
+chipsterWeb.controller('WorkflowGraphCtrl', ['$scope','$http', function($scope,$http){
+  
+      $http.get('js/json/workflow.json')
+        .then(function(res){
+          $scope.d3Data=res.data;
+        });
 
-      $scope.d3Data = [{
-        "nodes":[
-                  {"name":"bam1","group":1,x:200,y:150},
-                  {"name":"pdf","group":2,x:140,y:300},
-                  {"name":"log","group":2,x:300,y:300},
-                  {"name":"bed","group":3,x:300,y:180}
-                ],
-        "links":[
-                  {"source":0,"target":1,"weight":1},
-                  {"source":1,"target":2,"weight":3},
-                  {"source":2,"target":3,"weight":3}
-              ]
-            
-        }];
-       
-
-
-      //End of json file
 
     }]);
-
 
 
 
@@ -33,93 +14,135 @@ chipsterWeb.controller('WorkflowGraphCtrl', ['$scope', function($scope){
 
       //### Start Return Directive  ###
       return {
-      restrict: 'EA',
-      template:"<svg height='600' width='800'></svg>",
-      scope: {
-        data: "=",
-        onClick: "&"
+        restrict: 'EA',
+        template:"<svg height='800' width='800'></svg>",
+        scope: {
+          data: "=",
+          onClick: "&"
       },
       link: function(scope, iElement, iAttrs) {
 
-        // Calculate total nodes, max label length
         var d3=$window.d3;
-
         var c20 = d3.scale.category20();
-
-        graphData=scope.data[0];
-        console.log(graphData.nodes);
-
-
-
-        // ************** Generate the graph diagram  *****************
-        var margin = {top: 20, right: 120, bottom: 20, left: 120},
-            width = 800 - margin.right - margin.left,
-            height = 600 - margin.top - margin.bottom;
-            
         
-        var rawSvg=iElement.find('svg');
+        // We need to call the graph construction code after data is available
+        scope.$watch('data', function(data) {
+      
+          if(data){
+            graphData=data;
+
+           var rawSvg=iElement.find('svg');
+        
+          // Generate the graph diagram  
+            var margin = {top: 20, right: 120, bottom: 20, left: 120},
+                width = 800 - margin.right - margin.left,
+                height = 800 - margin.top - margin.bottom;
 
 
-        var svg = d3.select(rawSvg[0]).append("svg")
-            .attr("width", width + margin.right + margin.left)
-            .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var drag = d3.behavior.drag()
-              .on("drag", function(d,i) {
-                      d.x += d3.event.dx
-                      d.y += d3.event.dy
-                    d3.select(this).attr("cx", d.x).attr("cy",d.y);
-                    link.each(function(l,li){ 
-                    if(l.source==i){
-                      d3.select(this).attr("x1",d.x).attr("y1",d.y);        
-                    } else if(l.target==i){
-                      d3.select(this).attr("x2",d.x).attr("y2",d.y);
-                    } 
-                });
+          //Defining the force layout
+          var force= d3.layout.force()
+                        .charge(-120)
+                        .linkDistance(30)
+                        .size([width,height]);
+                         
+
+          var svg = d3.select(rawSvg[0]).append("svg")
+                .attr("width", width + margin.right + margin.left)
+                .attr("height", height + margin.top + margin.bottom);
+              
+                console.log(graphData.links);
+
+            //put the graph data onto force layout
+            force.nodes(graphData.nodes)
+                 .links(graphData.links)
+                 .start();
+
+            //create all the line svg but without location yet
+            var link= svg.selectAll(".link")
+                          .data(graphData.links)
+                          .enter().append("line")
+                          .attr("class","link")
+                          .style("marker-end","url(#suit)");
+
+            //create the nodes with circles
+            var node=svg.selectAll(".node")
+                        .data(graphData.nodes)
+                        .enter().append("circle")
+                        .attr("class","node")
+                        .attr("r",8)
+                        .style("fill",function(d){
+                          return c20(d.group)
+                        })
+                        .call(force.drag);
+
+
+            //give the SVG co-ordinates
+            force.on("tick",function(){
+              link.attr("x1",function(d){
+                    return d.source.x;
+              })
+                  .attr("y1",function(d){
+                    return d.source.y;
+                  })
+                  .attr("x2",function(d){
+                    return d.target.x;
+                  })
+                  .attr("y2",function(d){
+                    return d.target.y;
+                  });
+
+              node.attr("cx",function(d){
+                return d.x;
+              })
+                  .attr("cy",function(d){
+                    return d.y;
+              });
+
+
             });
 
+                              
 
-        var link=svg.selectAll(".link")
-                  .data(graphData.links)
-                  .enter().append("line")
-                  .attr("class","link")
-                  .attr("x1",function(l){
-                     var sourceNode=graphData.nodes.filter(function(d,i)
-                     { return i==l.source})[0];
-                     d3.select(this).attr("y1",sourceNode.y);
-                     return sourceNode.x
-                    })
-                  .attr("x2",function(l){
-                    var targetNode=graphData.nodes.filter(function(d,i)
-                      {return i==l.target})[0];
-                    d3.select(this).attr("y2",targetNode.y);
-                    return targetNode.x
-
-                  })
-                  .attr("fill","none")
-                  .attr("stroke","white");
-
-        var node=svg.selectAll(".node")
-                    .data(graphData.nodes)
-                    .enter().append("g")
-                    .append("circle")
-                    .attr("class","node")
-                    .attr("cx",function(d){return d.x})
-                    .attr("cy",function(d){return d.y})
-                    .attr("r",15)
-                    .attr("fill",function(d,i){return c20(i)})
-                    .append("text")
-                        .attr("dx",12)
-                        .attr("dy",".35em")
-                        .text(function(d){return d.name}) 
-                    .call(drag);
+            //Adding tooltip functionality
+            var tip=d3.tip()
+                      .attr("class","d3-tip")
+                      .offset([-10,0])
+                      .html(function(d){
+                        return d.name+"</span>";
+                      })
+                  svg.call(tip);
+            //End of adding tooltip
 
 
-          
+            //Adding Zoom function
 
-    }
+
+            //Adding arrow as links
+            svg.append("defs").selectAll("marker")
+               .data(["suit","licensing","resolved"])
+               .enter().append("marker")
+               .attr("id",function(d){ return d;})
+               .attr("viewBox","0 -5 10 10")
+               .attr("refX",25)
+               .attr("refY",0)
+               .attr("markerWidth",6)
+               .attr("markerHeight",6)
+               .attr("orient","auto")
+               .append("path")
+               .attr("d","M0,-5L10,0L0,5,L10,0,L0,-5")
+               .style("stroke","#4679BD")
+               .style("opacity","0.6");
+
+
+           
+
+    }//end of if
+
+  });//end of watch 
+
+
+  }//end of link function
 
 }
 //### End Return Directive  ###
