6b8c7194b2b3c9e55dc056c93d087e2b51b84077
Rumana Quazi <rquazi@gmail.com>
2015-07-15 13:10:30 +0300

    added Fisheye effect on graph nodes

--- js/controllers/WorkflowGraphCtrl.js
+++ js/controllers/WorkflowGraphCtrl.js
@@ -135,6 +135,34 @@ chipsterWeb.controller('WorkflowGraphCtrl', ['$scope','$http', function($scope,$
                .style("opacity","0.6");
 
 
+              //Adding fisheye effect
+
+              console.log(d3.fisheye);
+              var fisheye=d3.fisheye.circular()
+                            .radius(120);
+
+
+              svg.on("mousemove",function(){
+                force.stop();
+
+                fisheye.focus(d3.mouse(this));
+
+                d3.selectAll("circle").each(function(d){
+                  d.fisheye=fisheye(d);
+                })
+                  .attr("cx",function(d){ return d.fisheye.x;})
+                  .attr("cy",function(d){ return d.fisheye.y;})
+                  .attr("r", function(d){ return d.fisheye.z*8;});
+
+                  link.attr("x1",function(d){ return d.source.fisheye.x;})
+                      .attr("y1",function(d){ return d.source.fisheye.y;})
+                      .attr("x2",function(d){ return d.target.fisheye.x;})
+                      .attr("y2",function(d){ return d.target.fisheye.y;});
+
+              });
+
+
+
            
 
     }//end of if
