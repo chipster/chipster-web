0d9e641089ba05af01750159c629f1561ea6f630
Rumana Quazi <rquazi@gmail.com>
2015-08-05 16:19:45 +0300

    added microarray and ngs toolsets to the view

--- js/controllers/AnalysisToolTabCtrl.js
+++ js/controllers/AnalysisToolTabCtrl.js
@@ -1,19 +1,96 @@
 //Controller for different toolse
 chipsterWeb.controller('AnalysisToolTabCtrl', function($scope,$http){
 
-		this.tab = 1;
+	this.tab=1;
+	
+    $http.get('js/json/microarray.json')
+        .then(function(res){
+          console.log(res);
+          $scope.data = res.data;         
+      });
 
-        this.setTab = function (activeTab) {
+
+   	this.setTab = function (activeTab) {
             this.tab = activeTab;
             console.log('tab clicked');
+            if(activeTab==1){
+            	$http.get('js/json/microarray.json')
+        			.then(function(res){
+          			console.log(res);
+          			$scope.data = res.data;         
+      });
+            }
+            if(activeTab==2){
+            	$http.get('js/json/ngs.json')
+        		.then(function(res){
+          		console.log(res);
+          		$scope.data = res.data;         
+      });
+            }
 
         };
 
-        this.isSet = function (checkTab) {
+    this.isSet = function (checkTab) {
             return this.tab === checkTab;
         };
 
+     $scope.remove=function(scope){
+        scope.remove();
+      };
 
-	
+      $scope.toggle=function(scope){
+        console.log('toggle is called');
+        scope.toggle();
+      };
+
+      $scope.moveLastToTheBeginning=function(){
+        if($scope.data){
+          var a=$scope.data.pop();
+          $scope.data.splice(0,0,a);
+        }
+        
+      };
+
+      $scope.newSubItem=function(scope){
+        var nodeData=scope.$modelValue;
+        nodeData.nodes.push({
+          id:nodeData.id*10+nodeData.nodes.length,
+          title:nodeData.title + '.' + (nodeData.nodes.length + 1),
+          nodes:[]
+        });
+      };
+
+
+      $scope.collapseAll = function() {
+        $scope.$broadcast('collapseAll');//Dispathches the event to all child scopes(and their children) notifying the registered listeners
+      };
+
+      $scope.expandAll = function() {
+        $scope.$broadcast('expandAll');
+      };
+
+
+
+
+});
+
+chipsterWeb.factory('tools',function($http,$q){
+	return{
+		data:function(url){
+			//create a deffered object
+			var deferred=$q.defer();
+
+			//make the http call
+			$http.get(url).success(function(data){
+				console.log(url);
+				//when data is returned resolve the deferment
+			}).error(function(){
+				deferred.reject();
+			});
+
+			//return the the promise that work will be done.
+			return deferred.promise;
+		}
+	}
 
 });
