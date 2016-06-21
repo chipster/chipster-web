83100be173aafa00e0587c53e058ee998a4df5e9
klemela <klemela>
2016-06-21 11:57:47 +0300

    Build and update a browser automatically

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var browserSync = require('browser-sync').create();
+
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
+gulp.task("compile", function () {
     return tsProject.src()
         .pipe(ts(tsProject))
         .js.pipe(gulp.dest('dist/'));
 });
+
+gulp.task('start', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch('./app/**/*.ts', ['compile']);
+    gulp.watch("./dist/dist.js").on('change', browserSync.reload);
+    gulp.watch("./app/**/*.html").on('change', browserSync.reload);
+    gulp.watch("./app/**/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['start']);
