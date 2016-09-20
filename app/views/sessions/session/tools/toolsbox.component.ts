4ea1544b1f21de706b818150a46d558796d6332c
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-09-20 09:00:52 +0300

    Fix toolsbox.component.ts after merge

--- app/views/sessions/session/tools/toolsbox.component.ts
+++ app/views/sessions/session/tools/toolsbox.component.ts
@@ -1,18 +1,14 @@
 import SessionDataService from "../sessiondata.service";
 import ToolService from "./tool.service";
-import CSVReader from "../../../../services/csv/CSVReader"
 import Category from "../../../../model/session/category";
 import Module from "../../../../model/session/module";
 import Job from "../../../../model/session/job";
 import Dataset from "../../../../model/session/dataset";
 import Tool from "../../../../model/session/tool";
 import InputBinding from "../../../../model/session/inputbinding";
-import ToolParameter from "../../../../model/session/toolparameter";
-import JobParameter from "../../../../model/session/jobparameter";
 import SelectionService from "../selection.service";
 import {IChipsterFilter} from "../../../../common/filter/chipsterfilter";
 import Utils from "../../../../services/utils.service";
-import CSVModel from "../../../../services/csv/CSVModel";
 
 class ToolsBox {
 
@@ -113,85 +109,6 @@ class ToolsBox {
 
         // run
         this.SessionDataService.createJob(job);
-    }
-
-    toolSearchKeyEvent(e: any) {
-        if (e.keyCode == 13) { // enter
-            // select the first result
-            var visibleTools = this.$filter('toolFilter')(this.selectedCategory.tools, this.searchTool);
-            if (visibleTools[0]) {
-                this.searchTool = null;
-                this.selectTool(visibleTools[0].name.id);
-            }
-        }
-        if (e.keyCode == 27) { // escape key
-            // clear the search
-            this.searchTool = null;
-        }
-    }
-
-    getJobParameter(toolParameter: ToolParameter): JobParameter {
-
-        var jobParameter: JobParameter = {
-            parameterId: toolParameter.name.id,
-            displayName: toolParameter.name.displayName,
-            description: toolParameter.description,
-            type: toolParameter.type,
-            value: this.ToolService.getDefaultValue(toolParameter),
-            // access selectionOptions, defaultValue, optional, from and to values from the toolParameter
-            toolParameter: toolParameter
-        };
-
-        if (toolParameter.type === 'COLUMN_SEL') {
-            this.getColumns().then( function (columns: string[]) {
-                jobParameter.toolParameter.selectionOptions = columns.map( function (column) {
-                    return {id: column};
-                });
-            });
-        }
-
-        if (toolParameter.type === 'METACOLUMN_SEL') {
-            jobParameter.toolParameter.selectionOptions = this.getMetadataColumns().map( function (column) {
-                return {id: column};
-            });
-        }
-
-        return jobParameter;
-    }
-
-    getColumns() {
-
-        var promises: any[] = [];
-        for (let dataset of this.SelectionService.selectedDatasets) {
-            if (this.ToolService.isCompatible(dataset, 'TSV')) {
-                promises.push(this.CSVReader.getColumns(this.SessionDataService.getSessionId(), dataset.datasetId));
-            }
-        }
-
-        return this.$q.all(promises).then(function(columnsOfSelectedDatasets: string[][]) {
-            var columnSet = new Set<string>();
-            for (let columns of columnsOfSelectedDatasets) {
-                for (let column of columns) {
-                    columnSet.add(column);
-                }
-            }
-
-            return Array.from(columnSet);
-
-        }, function(e: any) {
-            console.log('failed to get columns', e);
-        });
-    }
-
-    getMetadataColumns() {
-
-        var keySet = new Set();
-        for (let dataset of this.SelectionService.selectedDatasets) {
-            for (let entry of dataset.metadata) {
-                keySet.add(entry.key);
-            }
-        }
-        return Array.from(keySet);
     }
 
     openToolsModal() {
