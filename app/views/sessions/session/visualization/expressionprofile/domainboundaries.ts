f6ffc2f28c5fcb2d63be78fb418eb069538864bd
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-10-07 10:53:51 +0300

    moved domainboundaries next to expressionprofile implementation

---
+++ app/views/sessions/session/visualization/expressionprofile/domainboundaries.ts
@@ -0,0 +1,11 @@
+export default class DomainBoundaries {
+
+    min: number;
+    max: number;
+
+    constructor(min: number, max: number) {
+        this.min = min;
+        this.max = max;
+    }
+
+}
