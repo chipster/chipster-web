1937225de70eef637afe9d1894584e8d038f7422
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-12-15 11:34:37 +0200

    Show html-visualization without scrolling

--- src/app/views/sessions/session/visualization/htmlvisualization/htmlvisualization.component.ts
+++ src/app/views/sessions/session/visualization/htmlvisualization/htmlvisualization.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'ch-htmlvisualization',
@@ -9,13 +11,30 @@ export class HtmlvisualizationComponent implements OnInit {
 
   @Input() src: string;
 
-  constructor() { }
+  private visualizationHTML: string;
 
-  ngOnInit() {}
+  constructor(private http: Http) { }
 
-  resize(htmlIframe: HTMLIFrameElement) {
-    htmlIframe.style.height = '1000px';
-    // htmlIframe.style.height = htmlIframe.contentWindow.document.body.scrollHeight + 'px';
+  ngOnInit() {
+    this.http.get(this.src).map((res:Response) => {
+      const html = res.text();
+      return html || '';
+    })
+      .catch( (error: Response | any) => {
+        let errMsg: string;
+        if (error instanceof Response) {
+          const body = error.json() || '';
+          const err = body.error || JSON.stringify(body);
+          errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+          errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+      })
+      .subscribe( (html: any) => {
+        this.visualizationHTML = html;
+      });
   }
 
 }
