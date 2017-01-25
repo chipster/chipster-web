68057e9f3547ea0a74a45b79df8d65774baa9b85
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-01-25 10:49:40 +0200

    Improve dataset parameters panel

--- src/app/views/sessions/session/selectiondetails/dataset-parameter-list/dataset-parameter-list.component.ts
+++ src/app/views/sessions/session/selectiondetails/dataset-parameter-list/dataset-parameter-list.component.ts
@@ -3,31 +3,45 @@ import JobParameter from "../../../../../model/session/jobparameter";
 
 @Component({
   selector: 'ch-dataset-parameter-list',
-  template: `<h5>Parameters {{parameters.length}} <button *ngIf="parameters.length > 3" (click)="toggleParameterList()" class="pull-right btn btn-xs btn-default">Show/Hide parameters</button></h5>
-                <table class="table table-condensed parameter-table">
-                      <tr *ngFor="let param of parameters; let i = index">
-                          <template [ngIf]="i < limit">
-                              <td>{{param.displayName}}</td>
-                              <td>{{param.value}}</td>
-                          </template>
-                      </tr>
-                </table>`
+  template: `<span class="h5">
+                Parameters
+                <span class="lighter" *ngIf="parameters.length > defaultLimit"> {{limit}} of {{parameters.length}}</span>
+             </span>
+             <span *ngIf="parameters.length > defaultLimit" ><a class="pull-right" (click)="toggleParameterList()">{{buttonText}}</a></span>
+                
+             <table class="table table-condensed parameter-table">
+                <tr *ngFor="let param of parameters; let i = index">
+                   <template [ngIf]="i < limit">
+                      <td>{{param.displayName}}</td>
+                         <td>{{param.value}}</td>
+                   </template>
+                </tr>
+             </table>`
 })
 export class DatasetParameterListComponent {
 
   @Input() parameters: Array<JobParameter>;
 
   private limit: number;
+  private defaultLimit: number = 3;
+  private buttonText: string;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.limit = 3;
+    this.limit = this.defaultLimit;
+    this.buttonText = 'Show all';
   }
 
   toggleParameterList() {
-    this.limit = this.limit === 3 ? this.parameters.length : 3;
+    if (this.limit === this.defaultLimit) {
+      this.limit = this.parameters.length;
+      this.buttonText = 'Hide';
+    } else {
+      this.limit = this.defaultLimit;
+      this.buttonText = 'Show all';
+    }
   }
 
 }
