0df4428d01d7b37e4bf8eaf946dfe2ad45cee946
klemela <klemela>
2017-02-13 12:03:25 +0200

    Remove angularjs remainings

--- src/app/upgradeadapter.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var upgrade_1 = require("@angular/upgrade");
-var app_module_1 = require("./app.module");
-exports.upgradeAdapter = new upgrade_1.UpgradeAdapter(app_module_1.AppModule);
-//# sourceMappingURL=upgradeadapter.js.map
