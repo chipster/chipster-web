b9f651f39805766a5c6415049a4d4a9ff68b0547
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-02-23 14:02:27 +0200

    Clear dataset selection when opening session

--- src/app/views/sessions/session/session.resolve.ts
+++ src/app/views/sessions/session/session.resolve.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import {SessionData} from "../../../model/session/session-data";
 import SessionResource from "../../../shared/resources/session.resource";
+import SelectionService from "./selection.service";
 
 @Injectable()
 export class SessionResolve implements Resolve<SessionData> {
 
-  constructor(private sessionResource: SessionResource) {}
+  constructor(private sessionResource: SessionResource,
+              private selectionService: SelectionService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
+    this.selectionService.clearSelection();
     return this.sessionResource.loadSession(route.params['sessionId']);
   }
 }
