ba307dd780a68e9a5dcb725fca9cb196c3f3e13d
klemela <klemela>
2017-03-21 13:17:20 +0200

    Angular-cli rc4 doesn't accept old code

--- src/app/views/sessions/session/joberrormodal/joberrormodal.controller.ts
+++ src/app/views/sessions/session/joberrormodal/joberrormodal.controller.ts
@@ -1,16 +1,16 @@
-JobErrorModalController.$inject = ['$uibModalInstance', 'toolErrorTitle', 'toolError'];
-
-function JobErrorModalController(
-    $uibModalInstance: any,
-    toolErrorTitle: string,
-    toolError: string) {
-
-    this.toolErrorTitle = toolErrorTitle;
-    this.toolError = toolError;
-
-    this.close = function () {
-        $uibModalInstance.close();
-    };
-};
-
-export default JobErrorModalController;
+// JobErrorModalController.$inject = ['$uibModalInstance', 'toolErrorTitle', 'toolError'];
+//
+// function JobErrorModalController(
+//     $uibModalInstance: any,
+//     toolErrorTitle: string,
+//     toolError: string) {
+//
+//     this.toolErrorTitle = toolErrorTitle;
+//     this.toolError = toolError;
+//
+//     this.close = function () {
+//         $uibModalInstance.close();
+//     };
+// };
+//
+// export default JobErrorModalController;
--- src/app/views/sessions/session/tools/inputsmodal/inputsmodal.controller.ts
+++ src/app/views/sessions/session/tools/inputsmodal/inputsmodal.controller.ts
@@ -1,46 +1,47 @@
-import Tool from "../../../../../model/session/tool";
-import Dataset from "../../../../../model/session/dataset";
-import InputBinding from "../../../../../model/session/inputbinding";
-import {ToolService} from "../tool.service";
-import * as _ from "lodash";
+// import Tool from "../../../../../model/session/tool";
+// import Dataset from "../../../../../model/session/dataset";
+// import InputBinding from "../../../../../model/session/inputbinding";
+// import {ToolService} from "../tool.service";
+// import * as _ from "lodash";
+//
+// export default class InputsModalController {
+//
+//     static $inject = ['$log', '$uibModalInstance', 'ToolService', 'selectedTool', 'moduleName','categoryName', 'inputBindings', 'selectedDatasets'];
+//
+//     constructor(
+//         private $log: ng.ILogService,
+//         private $uibModalInstance: any,
+//         private toolService: ToolService, // used by the template
+//         private selectedTool: Tool,
+//         private moduleName: string,
+//         private categoryName: string,
+//         private inputBindings: InputBinding[],
+//         private selectedDatasets: Dataset[]) {
+//     }
+//
+//     close() {
+//         this.$uibModalInstance.close({inputBindings: this.inputBindings});
+//     };
+//
+//
+//     inputSelected(changedBinding: InputBinding) {
+//
+//         // unselect new selection(s) from other bindings
+//         for (let inputBinding of this.inputBindings) {
+//             if (inputBinding != changedBinding) {
+//                 //let updated: Dataset[] = [];
+//                 for (let changed of changedBinding.datasets) {
+//                     for (let dataset of inputBinding.datasets) {
+//                         if (changed.datasetId != dataset.datasetId) {
+//                             //updated.push(dataset);
+//                         } else {
+//                             _.pull(inputBinding.datasets, dataset);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//
+// }
 
-export default class InputsModalController {
-
-    static $inject = ['$log', '$uibModalInstance', 'ToolService', 'selectedTool', 'moduleName','categoryName', 'inputBindings', 'selectedDatasets'];
-
-    constructor(
-        private $log: ng.ILogService,
-        private $uibModalInstance: any,
-        private toolService: ToolService, // used by the template
-        private selectedTool: Tool,
-        private moduleName: string,
-        private categoryName: string,
-        private inputBindings: InputBinding[],
-        private selectedDatasets: Dataset[]) {
-    }
-
-    close() {
-        this.$uibModalInstance.close({inputBindings: this.inputBindings});
-    };
-
-
-    inputSelected(changedBinding: InputBinding) {
-
-        // unselect new selection(s) from other bindings
-        for (let inputBinding of this.inputBindings) {
-            if (inputBinding != changedBinding) {
-                //let updated: Dataset[] = [];
-                for (let changed of changedBinding.datasets) {
-                    for (let dataset of inputBinding.datasets) {
-                        if (changed.datasetId != dataset.datasetId) {
-                            //updated.push(dataset);
-                        } else {
-                            _.pull(inputBinding.datasets, dataset);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-}
