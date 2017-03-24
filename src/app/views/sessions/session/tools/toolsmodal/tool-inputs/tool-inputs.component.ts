3d70da2a362753b6062209ad39a994278e987eb7
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-03-24 14:50:05 +0200

    Refactor tool inputs tab

---
+++ src/app/views/sessions/session/tools/toolsmodal/tool-inputs/tool-inputs.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {ToolResource} from "../../../../../../shared/resources/toolresource";
+import InputBinding from "../../../../../../model/session/inputbinding";
+import Tool from "../../../../../../model/session/tool";
+import {ToolService} from "../../tool.service";
+import Dataset from "../../../../../../model/session/dataset";
+
+@Component({
+  selector: 'ch-tool-inputs',
+  templateUrl: './tool-inputs.component.html',
+})
+export class ToolInputsComponent {
+
+  @Input() tool: Tool;
+  @Input() inputBindings: InputBinding[];
+  @Input() selectedDatasets: Dataset[];
+
+
+  constructor(private toolService: ToolService,
+              private toolResource: ToolResource) { }
+
+
+  inputSelected(changedBinding: InputBinding) {
+
+    // unselect selection(s) of changed binding from other bindings
+    for (let otherBinding of this.inputBindings.filter(inputBinding => inputBinding !== changedBinding)) {
+      for (let changed of changedBinding.datasets) {
+        for (let dataset of otherBinding.datasets) {
+          if (changed.datasetId === dataset.datasetId) {
+            _.pull(otherBinding.datasets, dataset);
+          }
+        }
+      }
+    }
+  }
+}
