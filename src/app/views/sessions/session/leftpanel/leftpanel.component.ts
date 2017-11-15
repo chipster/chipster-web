423609739c8e993bf2e149a1667bd243248e6f3e
klemela <klemela>
2017-11-15 09:53:51 +0200

    Keep the left side height constant even with a long session name

--- src/app/views/sessions/session/leftpanel/leftpanel.component.ts
+++ src/app/views/sessions/session/leftpanel/leftpanel.component.ts
@@ -2,14 +2,10 @@
 import {SessionDataService} from "../sessiondata.service";
 import Dataset from "../../../../model/session/dataset";
 import UtilsService from "../../../../shared/utilities/utils";
-import {SessionResource} from "../../../../shared/resources/session.resource";
-import {SessionWorkerResource} from "../../../../shared/resources/sessionworker.resource";
 import {SessionData} from "../../../../model/session/session-data";
 import {Component, Input} from "@angular/core";
-import {DialogModalService} from "../dialogmodal/dialogmodal.service";
 import {DatasetsearchPipe} from "../../../../shared/pipes/datasetsearch.pipe";
 import {SelectionHandlerService} from "../selection-handler.service";
-import {ErrorService} from "../../../error/error.service";
 import {SelectionService} from "../selection.service";
 
 @Component({
@@ -23,14 +19,11 @@ export class LeftPanelComponent {
 
   datasetSearch: string;
 
+  // noinspection JSUnusedLocalSymbols
   constructor(
-    private sessionResource: SessionResource,
-    private sessionDataService: SessionDataService,
-    private sessionWorkerResource: SessionWorkerResource,
-    private dialogModalService: DialogModalService,
+    private sessionDataService: SessionDataService, // used by template
     private datasetsearchPipe: DatasetsearchPipe,
     private selectionHandlerService: SelectionHandlerService,
-    private errorService: ErrorService,
     private selectionService: SelectionService) {} // used by template
 
   datasetSearchKeyEvent(e: any) {
@@ -61,72 +54,5 @@ export class LeftPanelComponent {
     } else {
       this.selectionHandlerService.setDatasetSelection([dataset]);
     }
-  }
-
-  renameSessionModal() {
-    this.dialogModalService.openSessionNameModal('Rename session', this.sessionData.session.name).then(name => {
-      console.log('renameSessionModal', name);
-      this.sessionData.session.name = name;
-      this.sessionDataService.updateSession(this.sessionData.session).subscribe();
-    }, () => {
-      // modal dismissed
-    });
-  }
-
-  notesModal() {
-
-    this.dialogModalService.openNotesModal(this.sessionData.session).then(notes => {
-      this.sessionData.session.notes = notes;
-      this.sessionDataService.updateSession(this.sessionData.session).subscribe(() => {}, err => {
-        this.errorService.headerError('failed to update session notes: ' + err, true);
-      })
-    }, () => {
-      // modal dismissed
-    });
-  }
-
-  sharingModal() {
-    this.dialogModalService.openSharingModal(this.sessionData.session);
-  }
-
-  duplicateModal() {
-    this.dialogModalService.openSessionNameModal('Duplicate session', this.sessionData.session.name + '_copy').then(name => {
-      if (!name) {
-        name = 'unnamed session';
-      }
-
-      let copySessionObservable = this.sessionResource.copySession(this.sessionData, name);
-
-      this.dialogModalService.openSpinnerModal('Duplicate session', copySessionObservable);
-    }, () => {
-      // modal dismissed
-    });
-  }
-
-  saveSessionFileModal() {
-    this.sessionDataService.download(
-      this.sessionWorkerResource.getPackageUrl(this.sessionDataService.getSessionId()));
-  }
-
-  removeSessionModal() {
-    this.dialogModalService.openBooleanModal('Delete session', 'Delete session ' + this.sessionData.session.name + '?', 'Delete', 'Cancel').then(() => {
-      // delete the session only from this user (i.e. the rule)
-      this.sessionDataService.deletePersonalRules(this.sessionData.session).subscribe( () => {}, err => {
-        this.errorService.headerError('failed to delete the session: ' + err, true);
-      });
-    }, () => {
-      // modal dismissed
-    });
-  }
-
-  autoLayout() {
-    this.sessionData.datasetsMap.forEach(d => {
-      if (d.x || d.y) {
-        d.x = null;
-        d.y = null;
-
-        this.sessionDataService.updateDataset(d);
-      }
-    });
   }
 }
