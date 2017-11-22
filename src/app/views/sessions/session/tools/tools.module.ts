f5b141468628caf4fdbcdf5d2aaa3c15342b9711
klemela <klemela>
2017-11-22 13:07:24 +0200

    Delete unused code

--- src/app/views/sessions/session/tools/tools.module.ts
+++ src/app/views/sessions/session/tools/tools.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ToolService} from "./tool.service";
-import {ToolTitleComponent} from "./tooltitle.component";
-import {ToolListItemComponent} from "./toolsmodal/tool-list/tool-list-item/tool-list-item.component";
+import {ToolListItemComponent} from "./dropdowns/tool-list/tool-list-item/tool-list-item.component";
 import {ToolsComponent} from "./tools.component";
-import {ToolsModalComponent} from "./toolsmodal/toolsmodal.component";
 import {SharedModule} from "../../../../shared/shared.module";
 import {FormsModule} from "@angular/forms";
-import { ToolsParameterFormComponent } from './toolsmodal/tools-parameter-form/tools-parameter-form.component';
+import { ToolsParameterFormComponent } from './dropdowns/tools-parameter-form/tools-parameter-form.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import { ToolSourceComponent } from './toolsmodal/tool-source/tool-source.component';
-import { ToolInputsComponent } from './toolsmodal/tool-inputs/tool-inputs.component';
+import { ToolSourceComponent } from './dropdowns/tool-source/tool-source.component';
+import { ToolInputsComponent } from './dropdowns/tool-inputs/tool-inputs.component';
 import { FilterCompatibleDatasetsPipe } from './filter-compatible-datasets.pipe';
 import {ManualModule} from "../../../manual/manual.module";
-import {ToolListComponent} from "./toolsmodal/tool-list/tool-list.component";
+import {ToolListComponent} from "./dropdowns/tool-list/tool-list.component";
 import {ScrollerComponent} from "./scroller/scroller.component";
 
 @NgModule({
@@ -25,10 +23,8 @@ import {ScrollerComponent} from "./scroller/scroller.component";
     ManualModule
   ],
   declarations: [
-    ToolTitleComponent,
     ToolListItemComponent,
     ToolsComponent,
-    ToolsModalComponent,
     ToolsParameterFormComponent,
     ToolSourceComponent,
     ToolInputsComponent,
