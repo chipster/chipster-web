9dab06e7ac7d23e2e652c29459964fcebb9b46a8
klemela <klemela>
2017-12-20 13:33:33 +0200

    Open in new tab link for html view

--- src/app/views/sessions/session/visualization/htmlvisualization/htmlvisualization.component.ts
+++ src/app/views/sessions/session/visualization/htmlvisualization/htmlvisualization.component.ts
@@ -6,7 +6,18 @@ import {ErrorHandlerService} from "../../../../../core/errorhandler/error-handle
 
 @Component({
   selector: 'ch-htmlvisualization',
-  template: `<iframe #htmlframe *ngIf="src" width="100%" [src]="wrapperUrl + '?location=' + src  | trustedresource" scrolling="no" frameborder="0" (load)="run(htmlframe)"></iframe>`
+  template: `
+    <a href="{{linkSrc}}" target="_blank" class="pull-right">
+      <i class="fa fa-external-link" aria-hidden="true"></i>
+      Open in new tab
+    </a>
+    <iframe #htmlframe 
+            *ngIf="src" 
+            width="100%" 
+            [src]="wrapperUrl + '?location=' + src  | trustedresource" 
+            scrolling="no" 
+            frameborder="0" 
+            (load)="run(htmlframe)"></iframe>`
 })
 export class HtmlvisualizationComponent implements OnChanges {
 
@@ -14,6 +25,7 @@ export class HtmlvisualizationComponent implements OnChanges {
   private dataset: Dataset;
 
   private src: string;
+  private linkSrc: string;
   private wrapperUrl: string = 'assets/htmlvisualizationwrapper.html';
 
   constructor(
@@ -22,6 +34,7 @@ export class HtmlvisualizationComponent implements OnChanges {
 
   ngOnChanges() {
     this.sessionDataService.getDatasetUrl(this.dataset).subscribe(url => {
+      this.linkSrc = url;
       // we have to encode the url to get in one piece to the other side, because it contains
       // a query parameter itself
       this.src = encodeURIComponent(url);
