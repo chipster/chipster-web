665fbca086473203bd52e425aa42629b241d73ce
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-12-28 15:25:07 +0200

    Clean up subscriptions in text visualisation

--- src/app/views/sessions/session/visualization/textvisualization/textvisualization.component.ts
+++ src/app/views/sessions/session/visualization/textvisualization/textvisualization.component.ts
@@ -1,36 +1,31 @@
 import {FileResource} from "../../../../../shared/resources/fileresource";
 import {SessionDataService} from "../../sessiondata.service";
 import Dataset from "../../../../../model/session/dataset";
-import {Component, Input, OnChanges} from "@angular/core";
+import {Component, Input, OnChanges, OnDestroy} from "@angular/core";
 import {Response} from "@angular/http";
 import {VisualizationModalService} from "../visualizationmodal.service";
 import {RestErrorService} from "../../../../../core/errorhandler/rest-error.service";
+import {Subject} from "rxjs/Subject";
 
 @Component({
   selector: 'ch-text-visualization',
-  template: `
-    <p *ngIf="!(data === '' || data)"><i>{{status}}</i></p>
-    
-    <div *ngIf="data === '' || data">
-      <label *ngIf="!isCompleteFile()">Showing {{getSizeShown() | bytes}} of {{getSizeFull() | bytes}}</label>
-      <ch-link-button class="pull-right" (click)="showAll()" *ngIf="!isCompleteFile()">Show all</ch-link-button>
-      <pre>{{data}}</pre>
-    </div>
-  `,
-
+  templateUrl: './textvisualization.component.html',
   styles: [`
     pre {
       background-color: white;
     }
   `],
 })
-export class TextVisualizationComponent implements OnChanges {
+export class TextVisualizationComponent implements OnChanges, OnDestroy {
 
   @Input() dataset: Dataset;
   @Input() showFullData: boolean;
 
   private data: string;
   private status: string;
+
+  private unsubscribe: Subject<any> = new Subject();
+
 
   fileSizeLimit = 10 * 1024;
 
@@ -41,17 +36,26 @@ export class TextVisualizationComponent implements OnChanges {
   }
 
   ngOnChanges() {
+    // unsubscribe from possible previous subscriptions
+    this.unsubscribe.next();
+
     this.data = null;
-    this.status = "Loading data...";
+    this.status = "Loading data";
     let maxBytes = this.showFullData ? null : this.fileSizeLimit;
 
-    console.log('getData()');
-    this.fileResource.getData(this.sessionDataService.getSessionId(), this.dataset, maxBytes).subscribe((response: any) => {
-      this.data = response;
-    }, (error: Response) => {
-      this.status = "Loading data failed";
-      this.errorHandlerService.handleError(error, "Loading data failed");
-    });
+    this.fileResource.getData(this.sessionDataService.getSessionId(), this.dataset, maxBytes)
+      .takeUntil(this.unsubscribe)
+      .subscribe((response: any) => {
+        this.data = response;
+      }, (error: Response) => {
+        this.status = "Loading data failed";
+        this.errorHandlerService.handleError(error, "Loading data failed");
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   getSizeShown() {
