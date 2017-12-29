6e37f96af53e1fce531388eccb9925ce7c967f35
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-12-29 11:02:53 +0200

    Add unsubscribe and status to html and pdf visualizations

--- src/app/views/sessions/session/visualization/htmlvisualization/htmlvisualization.component.ts
+++ src/app/views/sessions/session/visualization/htmlvisualization/htmlvisualization.component.ts
@@ -1,46 +1,49 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy} from '@angular/core';
 import {timeout} from "d3-timer";
 import {SessionDataService} from "../../sessiondata.service";
 import Dataset from "../../../../../model/session/dataset";
 import {RestErrorService} from "../../../../../core/errorhandler/rest-error.service";
+import {Subject} from "rxjs/Subject";
 
 @Component({
   selector: 'ch-htmlvisualization',
-  template: `
-    <a href="{{linkSrc}}" target="_blank" class="pull-right">
-      <i class="fa fa-external-link" aria-hidden="true"></i>
-      Open in new tab
-    </a>
-    <iframe #htmlframe 
-            *ngIf="src" 
-            width="100%" 
-            [src]="wrapperUrl + '?location=' + src  | trustedresource" 
-            scrolling="no" 
-            frameborder="0" 
-            (load)="run(htmlframe)"></iframe>`
+  templateUrl: './htmlvisualization.component.html'
 })
-export class HtmlvisualizationComponent implements OnChanges {
+
+export class HtmlvisualizationComponent implements OnChanges, OnDestroy {
 
   @Input()
   private dataset: Dataset;
+  private unsubscribe: Subject<any> = new Subject();
+  private status: string;
 
   private src: string;
   private linkSrc: string;
-  private wrapperUrl: string = 'assets/htmlvisualizationwrapper.html';
+  private wrapperUrl: string = 'assets/htmlvisualizationwrapper.html';s
 
   constructor(
     private sessionDataService: SessionDataService,
     private errorHandlerService: RestErrorService) { }
 
   ngOnChanges() {
-    this.sessionDataService.getDatasetUrl(this.dataset).subscribe(url => {
-      this.linkSrc = url;
-      // we have to encode the url to get in one piece to the other side, because it contains
-      // a query parameter itself
-      this.src = encodeURIComponent(url);
-    }, (error: any) => {
-      this.errorHandlerService.handleError(error, "Loading html file failed");
-    });
+    this.status = "Loading html file...";
+
+    this.sessionDataService.getDatasetUrl(this.dataset)
+      .takeUntil(this.unsubscribe)
+      .subscribe(url => {
+        this.linkSrc = url;
+        // we have to encode the url to get in one piece to the other side, because it contains
+        // a query parameter itself
+        this.src = encodeURIComponent(url);
+      }, (error: any) => {
+        this.status = "Loading html file failed";
+        this.errorHandlerService.handleError(error, "Loading html file failed");
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   run(htmlframe) {
--- src/app/views/sessions/session/visualization/pdf-visualization/pdf-visualization.component.ts
+++ src/app/views/sessions/session/visualization/pdf-visualization/pdf-visualization.component.ts
@@ -1,14 +1,15 @@
-import {Component, OnChanges, Input} from '@angular/core';
+import {Component, OnChanges, Input, OnDestroy} from '@angular/core';
 import Dataset from "../../../../../model/session/dataset";
 import {SessionDataService} from "../../sessiondata.service";
 import {RestErrorService} from "../../../../../core/errorhandler/rest-error.service";
+import {Subject} from "rxjs/Subject";
 
 @Component({
   selector: 'ch-pdf-visualization',
   templateUrl: './pdf-visualization.component.html',
   styleUrls: ['./pdf-visualization.component.less'],
 })
-export class PdfVisualizationComponent implements OnChanges {
+export class PdfVisualizationComponent implements OnChanges, OnDestroy {
 
   @Input()
   dataset: Dataset;
@@ -18,18 +19,31 @@ export class PdfVisualizationComponent implements OnChanges {
   page: number;
   zoom: number;
 
+  private unsubscribe: Subject<any> = new Subject();
+
+  private status: string = "";
+
   constructor(private sessionDataService: SessionDataService,
               private errorHandlerService: RestErrorService) { }
 
   ngOnChanges() {
+    this.status = "Loading pdf file...";
     this.page = 1;
     this.zoom = 1;
 
-    this.sessionDataService.getDatasetUrl(this.dataset).subscribe(url => {
+    this.sessionDataService.getDatasetUrl(this.dataset)
+      .takeUntil(this.unsubscribe)
+      .subscribe(url => {
       this.src = url;
     }, (error: any) => {
+      this.status = "Loading pdf file failed";
       this.errorHandlerService.handleError(error, "Loading pdf file failed");
     });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   previousPage() {
