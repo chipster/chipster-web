79b39e41c68425210ce2f7e667aea367cef8cb3d
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-01-31 11:49:15 +0200

    Rename isodate pipe to localDate

---
+++ src/app/shared/pipes/local-date.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'localDate'
+})
+export class LocalDatePipe implements PipeTransform {
+
+  transform(date: any): string {
+    if (date === null || date === undefined) {
+      return "";
+    }
+
+    let d: Date;
+    if (date instanceof Date) {
+      d = date;
+    } else {
+      d = new Date(date);
+      if (!(d instanceof Date)) {
+        return "";
+      }
+    }
+    return d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {BytesPipe} from "./pipes/bytes.pipe";
 import {TrustedResourcePipe} from "./pipes/trustedresource.pipe";
-import { IsoDatePipe } from './pipes/iso-date.pipe';
+import { LocalDatePipe } from './pipes/local-date.pipe';
 import { DatasetsearchPipe } from './pipes/datasetsearch.pipe';
 import { ToolPipe } from './pipes/toolpipe.pipe';
 import {PipeService} from "./services/pipeservice.service";
@@ -39,7 +39,7 @@ import {AuthHttpClientService} from "./services/auth-http-client.service";
   declarations: [
     BytesPipe,
     TrustedResourcePipe,
-    IsoDatePipe,
+    LocalDatePipe,
     DatasetsearchPipe,
     ToolPipe,
     CategoryPipe,
@@ -64,7 +64,7 @@ import {AuthHttpClientService} from "./services/auth-http-client.service";
   exports: [
     BytesPipe,
     TrustedResourcePipe,
-    IsoDatePipe,
+    LocalDatePipe,
     DatasetsearchPipe,
     ToolPipe,
     CategoryPipe,
