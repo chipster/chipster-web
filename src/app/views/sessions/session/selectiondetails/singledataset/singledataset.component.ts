69a75534fc146bdc87ea7c14b70c5451584f2939
klemela <klemela>
2018-04-03 12:34:12 +0300

    Show dataset notes again

--- src/app/views/sessions/session/selectiondetails/singledataset/singledataset.component.ts
+++ src/app/views/sessions/session/selectiondetails/singledataset/singledataset.component.ts
@@ -1,29 +1,33 @@
 import Dataset from "../../../../../model/session/dataset";
 import {SessionDataService} from "../../sessiondata.service";
 import Job from "../../../../../model/session/job";
-import {Component, Input, Output, EventEmitter} from "@angular/core";
+import {Component, Input, Output, EventEmitter, ViewChild, OnChanges, OnInit} from "@angular/core";
 import {SessionData} from "../../../../../model/session/session-data";
 import Tool from "../../../../../model/session/tool";
+import { RestErrorService } from "../../../../../core/errorhandler/rest-error.service";
 
 @Component({
   selector: 'ch-single-dataset',
   templateUrl: './singledataset.html',
   styleUrls: ['./singledataset-component.less'],
 })
-export class SingleDatasetComponent {
+export class SingleDatasetComponent implements OnInit, OnChanges {
 
   @Input() dataset: Dataset;
   @Input() private jobs: Map<string, Job>;
   @Input() private sessionData: SessionData;
-  @Output() onDelete: EventEmitter<any> = new EventEmitter();
 
   sourceJob: Job;
-  private tool:Tool;
+  private tool: Tool;
   toolCategory: string;
   toolName: string;
 
+  notesPlaceholderInactive = 'click to edit';
+  notesPlaceholderActive = 'notes about this file';
 
-  constructor(private sessionDataService: SessionDataService) {
+  constructor(
+    private sessionDataService: SessionDataService,
+    private restErrorService: RestErrorService) {
   }
 
   ngOnInit() {
@@ -40,26 +44,28 @@ export class SingleDatasetComponent {
     this.getUsedToolFromToolset();
   }
 
-
-  deleteDatasets() {
-    this.onDelete.emit();
+  editNotes(input) {
+    input.placeholder = this.notesPlaceholderActive;
   }
 
-  exportDatasets() {
-    this.sessionDataService.exportDatasets([this.dataset]);
+  saveNotes(dataset: Dataset, input) {
+    this.sessionDataService.updateDataset(dataset).subscribe(
+      null,
+      err => this.restErrorService.handleError(err, 'saving notes failed')
+    );
+    input.placeholder = this.notesPlaceholderInactive;
   }
-
-
 
   getSourceJob(dataset: Dataset) {
     return this.sessionDataService.getJobById(dataset.sourceJob, this.jobs);
   }
 
-  getUsedToolFromToolset(){
+  getUsedToolFromToolset() {
     if (this.sourceJob) {
-      let i=this.sessionData.tools.findIndex(x=>x.name.id==this.sourceJob.toolId);
-      if(i!=-1) this.tool=this.sessionData.tools[i];
-      else{
+      const i = this.sessionData.tools.findIndex(x => x.name.id === this.sourceJob.toolId);
+      if (i !== -1) {
+        this.tool = this.sessionData.tools[i];
+      } else {
         console.log('No Tool found with this ID', this.sourceJob.toolId);
       }
     } else {
