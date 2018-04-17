1c14870c8eb2b3198410d6faeb1fa173b6328624
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-04-17 12:41:14 +0300

    Fix linting errors

--- src/app/shared/services/upload.service.ts
+++ src/app/shared/services/upload.service.ts
@@ -1,27 +1,29 @@
-import {Injectable, ChangeDetectorRef} from "@angular/core";
-import {SessionResource} from "../resources/session.resource";
-import {TokenService} from "../../core/authentication/token.service";
-import {ConfigService} from "./config.service";
-import {Observable} from 'rxjs/Rx';
+import { Injectable, ChangeDetectorRef } from "@angular/core";
+import { SessionResource } from "../resources/session.resource";
+import { TokenService } from "../../core/authentication/token.service";
+import { ConfigService } from "./config.service";
+import { Observable } from "rxjs/Rx";
 import Dataset from "../../model/session/dataset";
 
 declare let Flow: any;
 
 @Injectable()
 export class UploadService {
-
   constructor(
-    private ConfigService: ConfigService,
+    private configService: ConfigService,
     private tokenService: TokenService,
-    private sessionResource: SessionResource) {
-  }
+    private sessionResource: SessionResource
+  ) {}
 
-  getFlow(fileAdded: (file: any, event: any, flow: any) => any, fileSuccess: (file: any) => any) {
-    let flow = new Flow({
+  getFlow(
+    fileAdded: (file: any, event: any, flow: any) => any,
+    fileSuccess: (file: any) => any
+  ) {
+    const flow = new Flow({
       // continuation from different browser session not implemented
       testChunks: false,
-      method: 'octet',
-      uploadMethod: 'PUT',
+      method: "octet",
+      uploadMethod: "PUT",
       // upload the chunks in order
       simultaneousUploads: 1,
       // don't spend time between requests too often
@@ -42,17 +44,17 @@ export class UploadService {
       throw Error("flow.js not supported");
     }
 
-    flow.on('fileAdded', (file, event) => {
-      //console.log(file, event);
+    flow.on("fileAdded", (file, event) => {
+      // console.log(file, event);
       this.flowFileAdded(file, event, flow);
       fileAdded(file, event, flow);
     });
     // noinspection JSUnusedLocalSymbols
-    flow.on('fileSuccess', (file, message) => {
-      //console.log(file, message);
+    flow.on("fileSuccess", (file, message) => {
+      // console.log(file, message);
       fileSuccess(file);
     });
-    flow.on('fileError', (file, message) => {
+    flow.on("fileError", (file, message) => {
       console.log(file, message);
     });
 
@@ -61,9 +63,8 @@ export class UploadService {
 
   // noinspection JSMethodCanBeStatic
   private flowFileAdded(file: any, event: any, flow: any) {
-
     // each file has a unique target url
-    flow.opts.target = function (file: any) {
+    flow.opts.target = function(file: any) {
       return file.chipsterTarget;
     };
 
@@ -72,13 +73,14 @@ export class UploadService {
 
   startUpload(sessionId: string, file: any) {
     Observable.forkJoin(
-      this.ConfigService.getFileBrokerUrl(),
+      this.configService.getFileBrokerUrl(),
       this.createDataset(sessionId, file.name)
-
     ).subscribe((value: [string, Dataset]) => {
-      let url = value[0];
-      let dataset = value[1];
-      file.chipsterTarget = `${url}/sessions/${sessionId}/datasets/${dataset.datasetId}?token=${this.tokenService.getToken()}`;
+      const url = value[0];
+      const dataset = value[1];
+      file.chipsterTarget = `${url}/sessions/${sessionId}/datasets/${
+        dataset.datasetId
+      }?token=${this.tokenService.getToken()}`;
       file.chipsterSessionId = sessionId;
       file.chipsterDatasetId = dataset.datasetId;
       file.resume();
@@ -86,12 +88,14 @@ export class UploadService {
   }
 
   private createDataset(sessionId: string, name: string): Observable<Dataset> {
-    let d = new Dataset(name);
-    return this.sessionResource.createDataset(sessionId, d).map((datasetId: string) => {
-      d.datasetId = datasetId;
-      this.sessionResource.updateDataset(sessionId, d);
-      return d;
-    });
+    const d = new Dataset(name);
+    return this.sessionResource
+      .createDataset(sessionId, d)
+      .map((datasetId: string) => {
+        d.datasetId = datasetId;
+        this.sessionResource.updateDataset(sessionId, d);
+        return d;
+      });
   }
 
   /**
