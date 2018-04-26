7c18163eea150197d3c9f51ce0ecb7d22ff096f0
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-04-26 08:48:04 +0300

    Hide workflow zoom buttons when no datasets

--- src/app/views/sessions/session/leftpanel/leftpanel.component.ts
+++ src/app/views/sessions/session/leftpanel/leftpanel.component.ts
@@ -1,21 +1,19 @@
-
-import {SessionDataService} from "../sessiondata.service";
+import { SessionDataService } from "../sessiondata.service";
 import Dataset from "../../../../model/session/dataset";
 import UtilsService from "../../../../shared/utilities/utils";
-import {SessionData} from "../../../../model/session/session-data";
-import {Component, Input} from "@angular/core";
-import {DatasetsearchPipe} from "../../../../shared/pipes/datasetsearch.pipe";
-import {SelectionHandlerService} from "../selection-handler.service";
+import { SessionData } from "../../../../model/session/session-data";
+import { Component, Input } from "@angular/core";
+import { DatasetsearchPipe } from "../../../../shared/pipes/datasetsearch.pipe";
+import { SelectionHandlerService } from "../selection-handler.service";
 import { SelectionService } from "../selection.service";
 import * as _ from "lodash";
 
 @Component({
-  selector: 'ch-leftpanel',
-  templateUrl: './leftpanel.component.html',
-  styleUrls: ['./leftpanel.component.less'],
+  selector: "ch-leftpanel",
+  templateUrl: "./leftpanel.component.html",
+  styleUrls: ["./leftpanel.component.less"]
 })
 export class LeftPanelComponent {
-
   @Input() sessionData: SessionData;
 
   datasetSearch: string;
@@ -25,7 +23,8 @@ export class LeftPanelComponent {
     private sessionDataService: SessionDataService, // used by template
     private datasetsearchPipe: DatasetsearchPipe,
     private selectionHandlerService: SelectionHandlerService,
-    private selectionService: SelectionService) {} // used by template
+    private selectionService: SelectionService
+  ) {} // used by template
 
   search(value: any) {
     this.datasetSearch = value;
@@ -34,7 +33,9 @@ export class LeftPanelComponent {
   searchEnter() {
     // select highlighted datasets when the enter key is pressed
     const allDatasets = this.getDatasetList();
-    this.selectionHandlerService.setDatasetSelection(this.datasetsearchPipe.transform(allDatasets, this.datasetSearch));
+    this.selectionHandlerService.setDatasetSelection(
+      this.datasetsearchPipe.transform(allDatasets, this.datasetSearch)
+    );
     this.datasetSearch = null;
   }
 
@@ -48,11 +49,12 @@ export class LeftPanelComponent {
     the type tags are do exist.
     */
     // convert to array[[key1, value1], [key2, value2], ...] for filtering and back to map
-    return new Map(Array.from(this.sessionData.datasetsMap)
-      .filter(entry => {
+    return new Map(
+      Array.from(this.sessionData.datasetsMap).filter(entry => {
         const dataset = entry[1];
         return dataset.fileId != null;
-      }));
+      })
+    );
   }
 
   getDatasetList(): Dataset[] {
@@ -61,22 +63,24 @@ export class LeftPanelComponent {
 
   getDatasetListSorted(): Dataset[] {
     // sort by created date, oldest first (string comparison should do with the current date format)
-    return this.getDatasetList().sort((a, b) => UtilsService.compareStringNullSafe(a.created, b.created));
+    return this.getDatasetList().sort((a, b) =>
+      UtilsService.compareStringNullSafe(a.created, b.created)
+    );
   }
 
   toggleDatasetSelection($event: any, dataset: Dataset): void {
     if (UtilsService.isCtrlKey($event)) {
       this.selectionHandlerService.toggleDatasetSelection([dataset]);
       console.log([dataset]);
-
     } else if (UtilsService.isShiftKey($event)) {
-
       //  datasets and their ids in the order of the dataset list
       const allDatasets = this.getDatasetListSorted();
       const allIds = allDatasets.map(d => d.datasetId);
 
       // indexes of the old selection in the dataset list
-      const selectedIndexes = this.selectionService.selectedDatasets.map(d => allIds.indexOf(d.datasetId));
+      const selectedIndexes = this.selectionService.selectedDatasets.map(d =>
+        allIds.indexOf(d.datasetId)
+      );
       const clickIndex = allIds.indexOf(dataset.datasetId);
       const newMin = Math.min(clickIndex, ...selectedIndexes);
       const newMax = Math.max(clickIndex, ...selectedIndexes);
@@ -84,7 +88,6 @@ export class LeftPanelComponent {
       // datasets within the index range
       const newSelection = _.range(newMin, newMax + 1).map(i => allDatasets[i]);
       this.selectionHandlerService.setDatasetSelection(newSelection);
-
     } else {
       this.selectionHandlerService.setDatasetSelection([dataset]);
     }
