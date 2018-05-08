8da9f3cdac75bea4dc233f545f40744162f5e5a4
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-05-08 09:01:09 +0300

    Improve upload button and modal

--- src/app/views/sessions/session/leftpanel/upload/upload-modal.component.ts
+++ src/app/views/sessions/session/leftpanel/upload/upload-modal.component.ts
@@ -5,22 +5,31 @@ import {
   ChangeDetectorRef,
   ViewChild,
   AfterViewInit,
-  OnInit
+  OnInit,
+  OnDestroy
 } from "@angular/core";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 import { UploadService } from "../../../../../shared/services/upload.service";
 
 @Component({
   selector: "ch-add-dataset-modal-content",
-  templateUrl: "./upload-modal.component.html"
+  templateUrl: "./upload-modal.component.html",
+  styleUrls: ["./upload-modal.component.less"]
 })
-export class UploadModalComponent implements AfterViewInit, OnInit {
+export class UploadModalComponent implements AfterViewInit, OnInit, OnDestroy {
   @Input() sessionId: string;
+  @Input() flow: any;
 
-  @ViewChild("browseFilesButton") browseFilesButton;
-  @ViewChild("browseDirButton") browseDirButton;
+  @ViewChild("uploadFilesButton") uploadFilesButton;
+  @ViewChild("uploadFolderButton") uploadFolderButton;
 
-  flow;
+  files = [];
+
+  // function reference like this needed for the flow.off in ngOnDestroy
+  update = function() {
+    this.files = this.flow.files.slice().reverse();
+    // this.changeDetectorRef.detectChanges();
+  }.bind(this);
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -29,24 +38,24 @@ export class UploadModalComponent implements AfterViewInit, OnInit {
   ) {}
 
   ngOnInit() {
-    this.flow = this.uploadService.getFlow(
-      this.fileAdded.bind(this),
-      this.fileSuccess.bind(this)
-    );
+    this.flow.on("progress", this.update);
+    this.flow.on("fileRemoved", this.update);
+    this.flow.on("error", (message, file, chunk) => this.update);
   }
 
+  ngOnDestroy() {
+    this.flow.off("progress", this.update);
+    this.flow.off("fileRemoved", this.update);
+    this.flow.off("error", this.update);
+  }
+
+  // called by upload.component
   fileAdded(file: any) {
-    this.uploadService.scheduleViewUpdate(this.changeDetectorRef, this.flow);
     this.uploadService.startUpload(this.sessionId, file);
   }
 
-  fileSuccess(file: any) {
-    // remove from the list
-    file.cancel();
-  }
-
   ngAfterViewInit() {
-    this.flow.assignBrowse(this.browseFilesButton);
-    this.flow.assignBrowse(this.browseDirButton, true);
+    this.flow.assignBrowse(this.uploadFilesButton);
+    this.flow.assignBrowse(this.uploadFolderButton, true);
   }
 }
--- src/app/views/sessions/session/leftpanel/upload/upload.component.ts
+++ src/app/views/sessions/session/leftpanel/upload/upload.component.ts
@@ -1,6 +1,12 @@
 import Dataset from "../../../../../model/session/dataset";
-import { Component, Input, ViewChild, OnInit } from "@angular/core";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import {
+  Component,
+  Input,
+  ViewChild,
+  OnInit,
+  AfterViewInit
+} from "@angular/core";
+import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 import { UploadModalComponent } from "./upload-modal.component";
 import { UploadService } from "../../../../../shared/services/upload.service";
 
@@ -8,32 +14,70 @@ import { UploadService } from "../../../../../shared/services/upload.service";
   selector: "ch-add-dataset-modal",
   templateUrl: "./upload.component.html"
 })
-export class UploadComponent {
+export class UploadComponent implements AfterViewInit, OnInit {
   @Input() datasetsMap: Map<string, Dataset>;
   @Input() sessionId: string;
 
-  @ViewChild("browseFilesButton") browseFilesButton;
+  @ViewChild("uploadFilesButton") uploadFilesButton;
+  @ViewChild("uploadFolderButton") uploadFolderButton;
 
-  flow;
+  private flow: any;
+  private modalOpen = false;
+  private modalRef: any;
 
   constructor(
     private modalService: NgbModal,
-    private uploadService: UploadService
+    private uploadService: UploadService,
+    private activeModal: NgbActiveModal
   ) {}
 
-  // ngOnInit() {
-  //   this.flow = this.uploadService
-  //     .getFlow
-  //     // this.fileAdded.bind(this),
-  //     // this.fileSuccess.bind(this)
-  //     ();
-  // }
+  ngOnInit() {
+    this.flow = this.uploadService.getFlow(
+      this.fileAdded.bind(this),
+      this.fileSuccess.bind(this)
+    );
+  }
 
-  open() {
-    const modalRef = this.modalService.open(UploadModalComponent, {
-      size: "lg"
-    });
-    modalRef.componentInstance.datasetsMap = this.datasetsMap;
-    modalRef.componentInstance.sessionId = this.sessionId;
+  fileAdded(file: any) {
+    console.log("file added", file);
+
+    if (!this.modalOpen) {
+      console.log("opening upload modal");
+
+      this.modalRef = this.modalService.open(UploadModalComponent, {
+        size: "lg"
+      });
+
+      this.modalRef.componentInstance.sessionId = this.sessionId;
+      this.modalRef.componentInstance.flow = this.flow;
+
+      this.modalOpen = true;
+
+      this.modalRef.result.then(
+        result => {
+          this.modalOpen = false;
+        },
+        reason => {
+          this.modalOpen = false;
+        }
+      );
+    } else {
+      console.log("upload modal already open");
+    }
+
+    console.log("modal ref", this.modalRef);
+
+    this.modalRef.componentInstance.fileAdded(file);
+  }
+
+  fileSuccess(file: any) {
+    // console.log("file success");
+    // remove from the list
+    // file.cancel();
+  }
+
+  ngAfterViewInit() {
+    this.flow.assignBrowse(this.uploadFilesButton);
+    this.flow.assignBrowse(this.uploadFolderButton, true);
   }
 }
