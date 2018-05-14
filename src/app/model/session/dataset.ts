1d827885c436b24aad76108c487660dbb17dc163
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-05-14 11:56:26 +0300

    Fix linting errors

--- src/app/model/session/dataset.ts
+++ src/app/model/session/dataset.ts
@@ -1,21 +1,20 @@
 import MetadataEntry from "./metadataentry";
 
 export default class Dataset {
+  constructor(name: string) {
+    this.name = name;
+  }
 
-    constructor(name: string) {
-        this.name = name;
-    }
-
-    checksum: string;
-    created: string;
-    datasetId: string;
-    fileId: string;
-    metadata: MetadataEntry[];
-    typeTags: {[key:string]:string;};
-    name: string;
-    notes: string;
-    size: number;
-    sourceJob: string;
-    x: number;
-    y: number;
+  checksum: string;
+  created: string;
+  datasetId: string;
+  fileId: string;
+  metadata: MetadataEntry[];
+  typeTags: { [key: string]: string };
+  name: string;
+  notes: string;
+  size: number;
+  sourceJob: string;
+  x: number;
+  y: number;
 }
