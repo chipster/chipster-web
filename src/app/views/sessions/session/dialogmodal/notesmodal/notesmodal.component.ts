810e4088bd8f96fe53d85c1a4e2cb0aab39a2172
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-10-17 06:48:56 +0300

    Fix scroll to bottom when opening modals

--- src/app/views/sessions/session/dialogmodal/notesmodal/notesmodal.component.ts
+++ src/app/views/sessions/session/dialogmodal/notesmodal/notesmodal.component.ts
@@ -1,24 +1,30 @@
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {Component, Input, AfterViewInit, ViewChild} from "@angular/core";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { Component, Input, AfterViewInit, ViewChild } from "@angular/core";
 import { Session } from "chipster-js-common";
 
 @Component({
-  templateUrl: './notesmodal.component.html',
-  styleUrls: ['./notesmodal.component.less']
+  templateUrl: "./notesmodal.component.html",
+  styleUrls: ["./notesmodal.component.less"]
 })
-export class NotesModalComponent implements AfterViewInit{
+export class NotesModalComponent implements AfterViewInit {
+  @Input()
+  session: Session;
 
-  @Input() session: Session;
+  @ViewChild("submitButton")
+  submitButton;
 
-  @ViewChild('submitButton') submitButton;
-
-  constructor(
-    private activeModal: NgbActiveModal) {}
+  constructor(private activeModal: NgbActiveModal) {}
 
   ngAfterViewInit() {
     // set focus to submit button every time the dialog is opened
     // autofocus attribute would work only once when the component is created
-    this.submitButton.nativeElement.focus();
+    setTimeout(() => {
+      // needs setTimeout or the page scrolls to the bottom
+      // should be fixed in ng-bootstrap 3.3.0
+      // https://github.com/ng-bootstrap/ng-bootstrap/issues/1776
+      // https:github.com/ng-bootstrap/ng-bootstrap/issues/2728
+      this.submitButton.nativeElement.focus();
+    });
   }
 
   save() {
--- src/app/views/sessions/session/dialogmodal/stringmodal/stringmodal.component.ts
+++ src/app/views/sessions/session/dialogmodal/stringmodal/stringmodal.component.ts
@@ -1,25 +1,35 @@
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {Component, Input, AfterViewInit, ViewChild} from "@angular/core";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { Component, Input, AfterViewInit, ViewChild } from "@angular/core";
 
 @Component({
-  templateUrl: './stringmodal.component.html'
+  templateUrl: "./stringmodal.component.html"
 })
 export class StringModalComponent implements AfterViewInit {
+  @Input()
+  buttonText: string;
+  @Input()
+  description: string;
+  @Input()
+  value: string;
+  @Input()
+  title: string;
+  @Input()
+  placeHolder: string;
 
-  @Input() buttonText: string;
-  @Input() description: string;
-  @Input() value: string;
-  @Input() title: string;
-  @Input() placeHolder: string;
+  @ViewChild("valueInput")
+  valueInput;
 
-  @ViewChild('valueInput') valueInput;
-
-  constructor(
-    private activeModal: NgbActiveModal) {}
+  constructor(private activeModal: NgbActiveModal) {}
 
   ngAfterViewInit() {
-    this.valueInput.nativeElement.focus();
-    setTimeout(() => this.valueInput.nativeElement.select());
+    setTimeout(() => {
+      // needs setTimeout or the page scrolls to the bottom
+      // should be fixed in ng-bootstrap 3.3.0
+      // https://github.com/ng-bootstrap/ng-bootstrap/issues/1776
+      // https:github.com/ng-bootstrap/ng-bootstrap/issues/2728
+      this.valueInput.nativeElement.focus();
+      this.valueInput.nativeElement.select();
+    });
   }
 
   save() {
