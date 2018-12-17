28b2b777ddbab19066704f386d07f2851e1b94c0
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-12-17 13:21:43 +0200

    Add setBackupAppName to app name guard

--- src/app/core/routing/app-name-guard.service.ts
+++ src/app/core/routing/app-name-guard.service.ts
@@ -3,13 +3,14 @@ import { CanActivate, Router } from "@angular/router";
 import { ActivatedRouteSnapshot } from "@angular/router";
 import { RouterStateSnapshot } from "@angular/router";
 import log from "loglevel";
+import { RouteService } from "../../shared/services/route.service";
 
 /**
  *
  */
 @Injectable()
 export class AppNameGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private routeService: RouteService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -17,6 +18,7 @@ export class AppNameGuard implements CanActivate {
   ): boolean {
     const appName = route.url[0].path;
     if (appName === "chipster" || appName === "mylly") {
+      this.routeService.setBackupAppName(appName);
       return true;
     } else {
       log.warn("invalid appName", appName, "redirecting to chipster home");
