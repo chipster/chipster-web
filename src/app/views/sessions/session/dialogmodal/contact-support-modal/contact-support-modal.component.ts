651326a283d14020ae96b9655286bee317ca7608
klemela <klemela>
2018-12-20 09:55:46 +0200

    Copy session for the help request

--- src/app/views/sessions/session/dialogmodal/contact-support-modal/contact-support-modal.component.ts
+++ src/app/views/sessions/session/dialogmodal/contact-support-modal/contact-support-modal.component.ts
@@ -5,15 +5,21 @@ import {
   AfterViewInit,
   ViewChild,
   OnInit,
-  ChangeDetectorRef
+  ChangeDetectorRef,
+  Inject
 } from "@angular/core";
-import { Session, Rule, User } from "chipster-js-common";
+import { Session, Rule, User, Role } from "chipster-js-common";
 import { RestErrorService } from "../../../../../core/errorhandler/rest-error.service";
 import { SessionWorkerResource } from "../../../../../shared/resources/sessionworker.resource";
 import { UserService } from "../../../../../shared/services/user.service";
-import { mergeMap, tap } from "rxjs/operators";
+import { mergeMap, tap, map } from "rxjs/operators";
 import { SessionResource } from "../../../../../shared/resources/session.resource";
 import { AuthenticationService } from "../../../../../core/authentication/authentication-service";
+import { of, Observable } from "rxjs";
+import { SessionData } from "../../../../../model/session/session-data";
+import { ConfigService } from "../../../../../shared/services/config.service";
+import { DOCUMENT } from "@angular/platform-browser";
+import { RouteService } from "../../../../../shared/services/route.service";
 
 @Component({
   templateUrl: "./contact-support-modal.component.html",
@@ -29,6 +35,7 @@ export class ContactSupportModalComponent implements AfterViewInit, OnInit {
   public attach: string; // force user the the select
 
   public session: Session;
+  public user: User;
   public isVerifiedEmail = false;
   public formSubmitAttempt = false;
 
@@ -39,13 +46,20 @@ export class ContactSupportModalComponent implements AfterViewInit, OnInit {
     private userService: UserService,
     private sessionResource: SessionResource,
     private authenticationService: AuthenticationService,
+    private routeService: RouteService,
     private cdr: ChangeDetectorRef,
+    @Inject(DOCUMENT) private document,
   ) { }
 
   ngOnInit() {
 
     this.userService.getLatestSession().pipe(
-      mergeMap((sessionId: string) => this.sessionResource.getSession(sessionId)),
+      mergeMap((sessionId: string) => {
+        if (sessionId != null) {
+          return this.sessionResource.getSession(sessionId);
+        }
+        return of(null);
+      }),
       tap(session => {
         this.session = session;
         if (this.session == null) {
@@ -54,6 +68,7 @@ export class ContactSupportModalComponent implements AfterViewInit, OnInit {
       }),
       mergeMap(() => this.authenticationService.getUser()),
       tap((user: User) => {
+        this.user = user;
         if (user.mail != null) {
           this.email = user.mail;
           this.isVerifiedEmail = true;
@@ -69,7 +84,6 @@ export class ContactSupportModalComponent implements AfterViewInit, OnInit {
       // workaround because Angular template based forms
       // don't like programmatic changes https://github.com/angular/angular/issues/22426
       this.cdr.detectChanges();
-
     }, 0);
   }
 
@@ -78,13 +92,65 @@ export class ContactSupportModalComponent implements AfterViewInit, OnInit {
 
     if (form.valid) {
 
-      const sessionId = this.session ? this.session.sessionId : null;
+      let copySessionId$: Observable<string>;
 
-      this.sessionWorkerResource.supportRequest(this.message, sessionId, this.email)
-        .subscribe(resp => {
+      if (this.session != null && this.attach === "yes") {
+        copySessionId$ = this.copyToSupportSessions(this.session.sessionId);
+      } else {
+        copySessionId$ = of(null);
+      }
+
+      copySessionId$.pipe(
+        mergeMap((sessionUrl: string) => {
+          console.log("support session url", sessionUrl);
+          return this.sessionWorkerResource.supportRequest(this.message, sessionUrl, this.email);
+        }),
+      ).subscribe(resp => {
           this.activeModal.close(this.session.notes);
         }, err => this.restErrorService.handleError(err));
     }
+  }
+
+  /**
+   * returns the url of the new session
+   */
+  copyToSupportSessions(sessionId: string): Observable<string> {
+    let sessionData: SessionData;
+    let copySessionId: string;
+    const userId = this.user.auth + "/" + this.user.username;
+    const utcDate = new Date().toISOString().split('T')[0];
+    // get the url of the app server
+    // the current url is correct also when using "ng serve"
+    const appHostUrl = this.document.location.protocol + "//" + this.document.location.hostname + ":" + this.document.location.port;
+
+    // the "preview" version of the sessionData is enough
+    return this.sessionResource.loadSession(sessionId, true).pipe(
+      mergeMap((data: SessionData) => {
+        sessionData = data;
+        const name = utcDate + "_" + userId + "_" + sessionData.session.name;
+        return this.sessionResource.copySession(sessionData, name, false);
+      }),
+      mergeMap((id: string) => {
+        copySessionId = id;
+        // share the session to the special user
+        const rule = new Rule();
+        rule.readWrite = true;
+        //FIXME make configurable
+        rule.username = "jaas/support_session_owner";
+        return this.sessionResource.createRule(copySessionId, rule);
+      }),
+      // find out the ruleId of user's own rule to delete it
+      mergeMap(() => this.sessionResource.getSession(copySessionId)),
+      mergeMap((session: Session) => {
+        const usersRule = session.rules.find(r => r.username === userId);
+        return this.sessionResource.deleteRule(copySessionId, usersRule.ruleId);
+      }),
+      map(() => {
+        // return the url of the new session
+        const appRoute = this.routeService.getAppRouteCurrent();
+        return appHostUrl + "/" + appRoute + "/analyze/" + copySessionId;
+      }),
+    );
   }
 
   cancel() {
