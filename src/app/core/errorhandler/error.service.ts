9bc0c7cdf22348d74092c91c33263ef681467009
Taavi Hupponen <taavi.hupponen@csc.fi>
2019-01-08 12:43:40 +0200

    Log errors in ErrorService

--- src/app/core/errorhandler/error.service.ts
+++ src/app/core/errorhandler/error.service.ts
@@ -1,15 +1,22 @@
-import {Injectable} from "@angular/core";
-import {Subject} from "rxjs/Subject";
-import {ErrorMessage, ErrorButton} from "./errormessage";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs/Subject";
+import { ErrorMessage, ErrorButton } from "./errormessage";
+import log from "loglevel";
 
 @Injectable()
 export class ErrorService {
-
   // handle the errors in component, because it can access the router
   private errors$ = new Subject();
 
   showError(msg: string, err: Error) {
-    const errorMessage = new ErrorMessage(null, msg, true, [ErrorButton.Reload, ErrorButton.ContactSupport], [], err);
+    const errorMessage = new ErrorMessage(
+      null,
+      msg,
+      true,
+      [ErrorButton.Reload, ErrorButton.ContactSupport],
+      [],
+      err
+    );
     if (err) {
       errorMessage.links = [ErrorButton.ShowDetails];
     }
@@ -17,6 +24,7 @@ export class ErrorService {
   }
 
   showErrorObject(errorMessage: ErrorMessage) {
+    log.error(errorMessage);
     this.errors$.next(errorMessage);
   }
 
