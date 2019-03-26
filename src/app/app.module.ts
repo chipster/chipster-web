728c6e1f58873a4ebefb95bc9fa6dbb3080c4698
Taavi Hupponen <taavi.hupponen@csc.fi>
2019-03-26 13:49:40 +0200

    Organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,44 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, ErrorHandler, Injector } from "@angular/core";
-import { HttpModule } from "@angular/http";
-import { NavigationComponent } from "./views/navigation/navigation.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ErrorHandler, Injector, NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { LoginComponent } from "./views/login/login.component";
-import { SelectionService } from "./views/sessions/session/selection.service";
-import { HomeComponent } from "./views/home/home.component";
-import { SessionModule } from "./views/sessions/session/session.module";
-import { CoreModule } from "./core/core.module";
+import { HttpModule } from "@angular/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { StoreModule } from "@ngrx/store";
+import { HotkeyModule } from "angular2-hotkeys";
+import { ToastrModule } from "ngx-toastr";
+import { setAppInjector } from "./app-injector";
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { TokenService } from "./core/authentication/token.service";
-import { AppRoutingModule } from "./app-routing.module";
-import { StoreModule } from "@ngrx/store";
-import { ErrorComponent } from "./views/error/error.component";
+import { CoreModule } from "./core/core.module";
 import { AppErrorHandler } from "./core/errorhandler/apperrorhandler";
 import { ErrorService } from "./core/errorhandler/error.service";
-import { HttpClientModule } from "@angular/common/http";
-import { ManualModule } from "./views/manual/manual.module";
-import { setAppInjector } from "./app-injector";
-import { AdminModule } from "./views/admin/admin.module";
-import { TermsComponent } from "./views/terms/terms.component";
+import { RoutingModule } from "./core/routing/routing.module";
+import { ActionToastComponent } from "./shared/components/action-toast";
 import { SharedModule } from "./shared/shared.module";
-import { HotkeyModule } from "angular2-hotkeys";
-import { selectedJobs } from "./state/selectedJobs.reducer";
+import { latestSession } from "./state/latest-session.reducer";
 import { selectedDatasets } from "./state/selectedDatasets.reducer";
+import { selectedJobs } from "./state/selectedJobs.reducer";
 import {
-  validatedTool,
   selectedTool,
   selectedToolWithInputs,
+  selectedToolWithPopulatedParams,
   selectedToolWithValidatedInputs,
-  selectedToolWithPopulatedParams
+  validatedTool
 } from "./state/tool.reducer";
-import { latestSession } from "./state/latest-session.reducer";
-import { NotFoundComponent } from "./views/error/not-found.component";
-import { RoutingModule } from "./core/routing/routing.module";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ToastrModule } from "ngx-toastr";
-import { ActionToastComponent } from "./shared/components/action-toast";
+import { AdminModule } from "./views/admin/admin.module";
 import { ContactModule } from "./views/contact/contact.module";
+import { ErrorComponent } from "./views/error/error.component";
+import { NotFoundComponent } from "./views/error/not-found.component";
+import { HomeComponent } from "./views/home/home.component";
+import { LoginComponent } from "./views/login/login.component";
+import { ManualModule } from "./views/manual/manual.module";
+import { NavigationComponent } from "./views/navigation/navigation.component";
+import { SelectionService } from "./views/sessions/session/selection.service";
+import { SessionModule } from "./views/sessions/session/session.module";
+import { TermsComponent } from "./views/terms/terms.component";
 
 @NgModule({
   imports: [
