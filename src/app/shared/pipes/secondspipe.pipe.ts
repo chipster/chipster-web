84a8bb25638db789a0b34e5643f4dd5e45ce73cb
klemela <klemela>
2019-06-07 16:09:42 +0300

    Fix style errors

--- src/app/shared/pipes/secondspipe.pipe.ts
+++ src/app/shared/pipes/secondspipe.pipe.ts
@@ -1,20 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'secondspipe'
+  name: "secondspipe"
 })
 export class SecondsPipe implements PipeTransform {
-
-  transform(seconds: number|string): any {
-    if (isNaN(parseFloat(<string>seconds)) || !isFinite(<number>seconds))
-      return '-';
-    if (seconds === 0)
-      return '';
-    var units = [ 'seconds', 'minutes', 'hours' ], number = Math.floor(Math
-        .log(<number>seconds)
-      / Math.log(60));
-    return (<number>seconds / Math.pow(60, Math.floor(number))).toFixed(0) + ' '
-      + units[number];
+  transform(seconds: number | string): any {
+    if (isNaN(parseFloat(<string>seconds)) || !isFinite(<number>seconds)) {
+      return "-";
+    }
+    if (seconds === 0) {
+      return "";
+    }
+    const units = ["seconds", "minutes", "hours"];
+    const number = Math.floor(Math.log(<number>seconds) / Math.log(60));
+    return (
+      (<number>seconds / Math.pow(60, Math.floor(number))).toFixed(0) +
+      " " +
+      units[number]
+    );
   }
-
 }
