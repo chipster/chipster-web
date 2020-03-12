8d26d815db04e644e452f83d60220f22e6578805
Taavi Hupponen <taavi.hupponen@csc.fi>
2020-03-12 14:47:23 +0200

    Fix file history view

--- src/app/views/sessions/session/selectiondetails/dataset-history-modal/dataset-history-modal.component.ts
+++ src/app/views/sessions/session/selectiondetails/dataset-history-modal/dataset-history-modal.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnChanges, OnInit } from "@angular/core";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 import { Dataset, Job, JobInput, JobParameter } from "chipster-js-common";
 import { SessionData } from "../../../../../model/session/session-data";
+import { QuerySessionDataService } from "../../query-session-data.service";
 
 export class DatasetHistoryStep {
   datasetName: string;
-  creationDate: string;
+  date: string;
   sourceJobName: string;
   sourceJob: Job;
   parameterList: JobParameter[];
@@ -23,7 +24,7 @@ interface HistoryOption {
   templateUrl: "./dataset-history-modal.component.html",
   styleUrls: ["./dataset-history-modal.component.less"]
 })
-export class DatasetHistorymodalComponent implements OnInit {
+export class DatasetHistorymodalComponent implements OnInit, OnChanges {
   @Input() dataset: Dataset;
   @Input() sessionData: SessionData;
 
@@ -39,62 +40,57 @@ export class DatasetHistorymodalComponent implements OnInit {
 
   public historyOptionsMap: Map<string, HistoryOption>;
 
-  stepCount: number;
   datasetHistorySteps: Array<DatasetHistoryStep> = [];
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(
+    public activeModal: NgbActiveModal,
+    public querySessionDataService: QuerySessionDataService
+  ) {}
 
   ngOnInit(): void {
     this.historyOptionsMap = new Map();
     this.historyOptions.forEach(option =>
       this.historyOptionsMap.set(option.id, option)
     );
+    this.datasetHistorySteps = this.getHistoryData();
+  }
 
-    this.stepCount = 0;
-    this.getHistoryData();
+  ngOnChanges(): void {
+    this.datasetHistorySteps = this.getHistoryData();
   }
 
   updateOption(option: HistoryOption): void {
     option.enabled = !option.enabled;
   }
 
-  getHistoryData(): void {
-    // TODO refactor
+  private getHistoryData(): Array<DatasetHistoryStep> {
+    const historySteps: Array<
+      DatasetHistoryStep
+    > = this.querySessionDataService
+      .getAncestorDatasetsBottomUpBreadthFirst(this.sessionData, this.dataset)
+      .map(dataset => {
+        const sourceJob = this.sessionData.jobsMap.get(dataset.sourceJob);
 
-    let sourceJobId: string = this.dataset.sourceJob;
-    let datasetId: string = this.dataset.datasetId;
-    let curSourceJob: Job;
+        return {
+          datasetName: dataset.name,
+          date: sourceJob != null ? sourceJob.created : dataset.created,
+          sourceJobName:
+            sourceJob != null
+              ? sourceJob.toolCategory + " / " + sourceJob.toolName
+              : "not available",
+          sourceJob: sourceJob,
+          parameterList: sourceJob != null ? sourceJob.parameters : [],
+          inputFileNamesString:
+            sourceJob != null
+              ? sourceJob.inputs
+                  .map((jobInput: JobInput) => jobInput.displayName)
+                  .join(" ")
+              : "",
+          sourceCode: sourceJob != null ? sourceJob.sourceCode : null
+        };
+      });
 
-    // if this is not the first input dataset, otherwise sourceJob is null or
-    // there will be no input file, so we need to check both
-
-    while (
-      this.sessionData.datasetsMap.has(datasetId) &&
-      this.sessionData.jobsMap.has(sourceJobId) &&
-      this.sessionData.jobsMap.get(sourceJobId).inputs.length > 0
-    ) {
-      const curStepDetail: DatasetHistoryStep = new DatasetHistoryStep();
-      // Populate history data
-      curSourceJob = this.sessionData.jobsMap.get(sourceJobId);
-      curStepDetail.datasetName = this.sessionData.datasetsMap.get(
-        datasetId
-      ).name;
-      curStepDetail.creationDate = this.sessionData.session.created;
-      curStepDetail.sourceJob = curSourceJob;
-      curStepDetail.sourceJobName = curSourceJob.toolName;
-      curStepDetail.parameterList = curSourceJob.parameters;
-      curStepDetail.inputFileNamesString = curSourceJob.inputs
-        .map((jobInput: JobInput) => jobInput.displayName)
-        .join(" ");
-      curStepDetail.sourceCode = curSourceJob.sourceCode;
-      this.datasetHistorySteps[this.stepCount] = curStepDetail;
-      this.stepCount++;
-
-      datasetId = this.sessionData.jobsMap.get(sourceJobId).inputs[0].datasetId;
-      sourceJobId = this.sessionData.datasetsMap.get(datasetId).sourceJob;
-    }
-
-    // Reverse the order of steps so that it shows analysis history from root to leaf
-    this.datasetHistorySteps.reverse();
+    // reverse the order of steps so that oldest comes first
+    return historySteps.reverse();
   }
 }
