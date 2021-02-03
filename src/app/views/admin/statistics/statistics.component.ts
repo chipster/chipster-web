e2794713b5d1f1fbed892bc5ba63026d141fa7f6
Taavi Hupponen <taavi.hupponen@csc.fi>
2021-02-03 19:56:23 +0200

    Implement basic statistics

--- src/app/views/admin/statistics/statistics.component.ts
+++ src/app/views/admin/statistics/statistics.component.ts
@@ -1,4 +1,13 @@
+import { HttpParams } from "@angular/common/http";
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { Role } from "chipster-js-common";
+import { flatMap } from "rxjs/operators";
+import { TokenService } from "../../../core/authentication/token.service";
+import { RestErrorService } from "../../../core/errorhandler/rest-error.service";
+import { LoadState } from "../../../model/loadstate";
+import { AuthHttpClientService } from "../../../shared/services/auth-http-client.service";
+import { ConfigService } from "../../../shared/services/config.service";
 
 @Component({
   selector: "ch-statistics",
@@ -7,7 +16,81 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
   encapsulation: ViewEncapsulation.Emulated
 })
 export class StatisticsComponent implements OnInit {
-  constructor() {}
+  readonly IGNORE_USERS_PARAMS = "ignoreUsers";
 
-  ngOnInit() {}
+  years = ["2020", "2021", "2022"];
+  yearControl = new FormControl(this.years[1]);
+  ignoreUsersControl = new FormControl("");
+
+  form = new FormGroup({
+    year: this.yearControl,
+    ignoreUsers: this.ignoreUsersControl
+  });
+
+  userCount: number;
+  jobCount: number;
+
+  state = LoadState.Ready;
+
+  constructor(
+    private configService: ConfigService,
+    private errorHandlerService: RestErrorService,
+    private auhtHttpClient: AuthHttpClientService,
+    private tokenService: TokenService
+  ) {}
+
+  ngOnInit(): void {
+    this.configService.get(ConfigService.KEY_STATISTICS_IGNORE_USERS).subscribe(
+      usersString => {
+        if (usersString) {
+          this.ignoreUsersControl.setValue(usersString);
+        }
+      },
+      err => {
+        this.errorHandlerService.showError(
+          "Failed to get ignoreUsers default",
+          err
+        );
+      }
+    );
+  }
+
+  public onSubmit(): void {
+    this.updateStats();
+  }
+
+  updateStats(): void {
+    this.state = LoadState.Loading;
+    let params = new HttpParams().append("year", this.yearControl.value);
+
+    this.ignoreUsersControl.value
+      .split(",")
+      .map(user => user.trim())
+      .filter(user => user.length > 0)
+      .forEach(user => {
+        params = params.append(this.IGNORE_USERS_PARAMS, user);
+      });
+
+    this.configService
+      .getInternalService(Role.JOB_HISTORY, this.tokenService.getToken())
+      .pipe(
+        flatMap(service => {
+          return this.auhtHttpClient.getAuthWithParams(
+            service.adminUri + "/admin/jobhistory/statistics",
+            params
+          );
+        })
+      )
+      .subscribe(
+        result => {
+          this.userCount = result["userCount"];
+          this.jobCount = result["jobCount"];
+          this.state = LoadState.Ready;
+        },
+        err => {
+          this.state = LoadState.Fail;
+          this.errorHandlerService.showError("get statistics failed", err);
+        }
+      );
+  }
 }
