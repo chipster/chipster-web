0529aaf1b9fde0299ab0f32d0ef15d4f6d11f269
Taavi Hupponen <taavi.hupponen@csc.fi>
2021-02-26 15:24:50 +0200

    Update angular 7 -> 8

--- src/app/views/contact/contact-support-modal/contact-support-modal.component.ts
+++ src/app/views/contact/contact-support-modal/contact-support-modal.component.ts
@@ -12,7 +12,7 @@ import {
   FormControl,
   Validators
 } from "@angular/forms";
-import { DOCUMENT } from "@angular/platform-browser";
+import { DOCUMENT } from "@angular/common";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 import { Rule, Session, User } from "chipster-js-common";
 import log from "loglevel";
