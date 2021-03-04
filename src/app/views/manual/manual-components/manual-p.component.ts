caadebb7e144d6b3ecf355831618a474cfc6e0e8
Petri Klemela <petri.klemela@csc.fi>
2021-03-04 13:31:19 +0200

    Simpler ManualComponent

--- src/app/views/manual/manual-components/manual-p.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-import { ManualBaseComponent } from "./manual-base.component";
-
-@Component({
-  selector: "ch-manual-p-component",
-  template: "<p #element><ng-template #container></ng-template></p>"
-})
-export class ManualPComponent extends ManualBaseComponent {}
--- src/app/views/manual/manual-components/manual-span.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-import { ManualBaseComponent } from "./manual-base.component";
-
-@Component({
-  selector: "ch-manual-div-component",
-  template: "<span #element><ng-template #container></ng-template></span>"
-})
-export class ManualSpanComponent extends ManualBaseComponent {}
