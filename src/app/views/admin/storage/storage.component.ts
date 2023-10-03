19e853063d68eca90dca70ba7d250a3fd92a7e6e
Taavi Hupponen <taavi.hupponen@csc.fi>
2023-10-03 17:02:23 +0300

    Get started

--- src/app/views/admin/storage/storage.component.ts
+++ src/app/views/admin/storage/storage.component.ts
@@ -5,6 +5,7 @@ import { Role } from "chipster-js-common";
 import log from "loglevel";
 import { forkJoin, of } from "rxjs";
 import { catchError, mergeMap, tap } from "rxjs/operators";
+import { AuthenticationService } from "../../../core/authentication/authentication-service";
 import { TokenService } from "../../../core/authentication/token.service";
 import { RestErrorService } from "../../../core/errorhandler/rest-error.service";
 import { LoadState, State } from "../../../model/loadstate";
@@ -23,7 +24,11 @@ export class StorageComponent implements OnInit {
   quotasMap: Map<string, any>;
 
   columnDefs: ColDef[] = [
-    { field: "username", sortable: true, filter: true },
+    { field: "user", sortable: true, filter: true },
+    { field: "auth", sortable: true, filter: true },
+    { field: "username", sortable: true, filter: true, pinned: "left" },
+    { field: "created", sortable: true, filter: "agDateColumnFilter" },
+    { field: "modified", sortable: true, filter: "agDateColumnFilter" },
     { field: "readWriteSessions", sortable: true },
     { field: "readOnlySessions", sortable: true },
     {
@@ -51,7 +56,8 @@ export class StorageComponent implements OnInit {
     private authHttpClient: AuthHttpClientService,
     private modalService: NgbModal,
     private bytesPipe: BytesPipe,
-    private tokenService: TokenService
+    private tokenService: TokenService,
+    private authenticationService: AuthenticationService
   ) {}
 
   ngOnInit() {
@@ -59,47 +65,107 @@ export class StorageComponent implements OnInit {
     this.users = [];
     this.quotasMap = new Map();
 
+    const authUsers$ = this.authenticationService.getUsers();
+
     let sessionDbUrl: string;
     let sessionDbAdminUrl: string;
     // check if its working properly
-    this.configService
-      .getInternalService(Role.SESSION_DB, this.tokenService.getToken())
-      .pipe(
-        tap((service) => {
-          sessionDbAdminUrl = service.adminUri;
-        }),
-        mergeMap(() => this.configService.getSessionDbUrl()),
-        tap((url) => {
-          sessionDbUrl = url;
-        }),
-        mergeMap(() => this.authHttpClient.getAuth(sessionDbUrl + "/users")),
-        tap((users: string[]) => {
-          this.users = users;
-        }),
-        mergeMap((users: string[]) => {
-          const userQuotas$ = users.map((user: string) => {
-            const url = sessionDbAdminUrl + "/admin/users/quota?userId=" + encodeURIComponent(user);
-            return this.authHttpClient.getAuth(url).pipe(
-              catchError((err) => {
-                log.error("quota request error", err);
-                // don't cancel other requests even if one of them fails
-                return of({
-                  username: user,
-                });
-              })
-            );
+    const sessionDbUsers$ = this.configService.getInternalService(Role.SESSION_DB, this.tokenService.getToken()).pipe(
+      tap((service) => {
+        sessionDbAdminUrl = service.adminUri;
+      }),
+      mergeMap(() => this.configService.getSessionDbUrl()),
+      tap((url) => {
+        sessionDbUrl = url;
+      }),
+      mergeMap(() => this.authHttpClient.getAuth(sessionDbUrl + "/users")),
+      tap((users: string[]) => {
+        this.users = users;
+      }),
+      mergeMap((users: string[]) => {
+        const userQuotas$ = users.map((user: string) => {
+          const url = sessionDbAdminUrl + "/admin/users/quota?userId=" + encodeURIComponent(user);
+          return this.authHttpClient.getAuth(url).pipe(
+            catchError((err) => {
+              log.error("quota request error", err);
+              // don't cancel other requests even if one of them fails
+              return of({
+                username: user,
+              });
+            })
+          );
+        });
+        return forkJoin(userQuotas$);
+      })
+    );
+    // sessionDbUsers$.subscribe({
+    //   next: (quotas) => {
+    //     quotas.forEach((quota) => this.quotasMap.set(quota.username, quota));
+    //     this.rowData = quotas;
+    //     this.allSessionsState = LoadState.Ready;
+    //   },
+    //   error: (err) => this.restErrorService.showError("get quotas failed", err),
+    // });
+
+    forkJoin([authUsers$, sessionDbUsers$]).subscribe({
+      next: ([authUsers, sessionDbUsers]) => {
+        sessionDbUsers.forEach((quota) => this.quotasMap.set(quota.username, quota));
+
+        // sessionDbUsers to a set
+        const sessionDbUsersSet = new Set();
+        sessionDbUsers.forEach((sessionDbUser) => {
+          sessionDbUsersSet.add(sessionDbUser.username);
+        });
+
+        // authUsers to a set
+        const authUsersSet = new Set();
+        authUsers.forEach((authUser) => {
+          authUsersSet.add(authUser.auth + "/" + authUser.username);
+        });
+
+        // sessionDbUsers to a map
+        const sessionDbUsersMap = new Map();
+        sessionDbUsers.forEach((sessionDbUser) => {
+          sessionDbUsersMap.set(sessionDbUser.username, sessionDbUser);
+        });
+
+        // authUsers to a map
+        const authUsersMap = new Map();
+        authUsers.forEach((authUser) => {
+          authUsersMap.set(authUser.auth + "/" + authUser.username, authUser);
+        });
+
+        // get intersection of both sets
+        const intersection = new Set([...sessionDbUsersSet].filter((x) => authUsersSet.has(x)));
+
+        // get difference of both sets
+        const difference = new Set([...sessionDbUsersSet].filter((x) => !authUsersSet.has(x)));
+
+        console.log("authUsers", authUsersSet);
+        console.log("sessionDbUsers", sessionDbUsersSet);
+
+        const combinedUsers = sessionDbUsers
+          .filter((sessionDbUser) => intersection.has(sessionDbUser.username))
+          .map((sessionDbUser) => {
+            // combine sessionDbUser properties and corresponding authUser properties
+            // both have username so order matters!
+            console.log(sessionDbUser.modified);
+            return {
+              ...authUsersMap.get(sessionDbUser.username),
+              ...sessionDbUser,
+              user: authUsersMap.get(sessionDbUser.username).username,
+              modified: new Date(authUsersMap.get(sessionDbUser.username).modified),
+              created: new Date(authUsersMap.get(sessionDbUser.username).created),
+            };
           });
-          return forkJoin(userQuotas$);
-        })
-      )
-      .subscribe({
-        next: (quotas) => {
-          quotas.forEach((quota) => this.quotasMap.set(quota.username, quota));
-          this.rowData = quotas;
-          this.allSessionsState = LoadState.Ready;
-        },
-        error: (err) => this.restErrorService.showError("get quotas failed", err),
-      });
+        console.log(combinedUsers);
+        const missingUsers = [];
+        sessionDbUsers.forEach((sessionDbUser) => {});
+        this.rowData = combinedUsers;
+        this.allSessionsState = LoadState.Ready;
+      },
+      error: (err) => this.restErrorService.showError("get users and quotas failed", err),
+    });
   }
 
   selectUser(user: string) {
