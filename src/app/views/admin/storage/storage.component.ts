eb956b4a7768e39dd7b69b6d73f1cdbc71a9ceb8
Taavi Hupponen <taavi.hupponen@csc.fi>
2023-12-07 01:38:11 +0200

    Fix unitTest -> guest in admin storage

--- src/app/views/admin/storage/storage.component.ts
+++ src/app/views/admin/storage/storage.component.ts
@@ -356,7 +356,7 @@ export class StorageComponent implements OnInit {
     };
 
     const oldGuestsFilter = {
-      userId: { type: "startsWith", filter: "jaas/UnitTest" },
+      userId: { type: "startsWith", filter: "jaas/guest" },
       modified: oldGuestsConditions,
     };
     gridApi.setFilterModel(oldGuestsFilter);
