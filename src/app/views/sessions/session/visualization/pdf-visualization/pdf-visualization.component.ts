eea01666820d34c3fa50d667126a2229430f5c77
Petri Klemela <petri.klemela@csc.fi>
2024-01-29 10:29:53 +0200

    Workaround for the zero pdf height

--- src/app/views/sessions/session/visualization/pdf-visualization/pdf-visualization.component.ts
+++ src/app/views/sessions/session/visualization/pdf-visualization/pdf-visualization.component.ts
@@ -22,6 +22,12 @@ export class PdfVisualizationComponent implements OnChanges, OnDestroy {
   zoom: number;
   showAll = false;
 
+  /* any positive value will do, the height of the component will be fixed
+  after the page is rendered. Cannot be 0, otherwise page won't be rendered. */
+  height = 500;
+  // add one dummy item, because page numbers start from 1
+  unscaledPageHeights = [0];
+
   loadedBytes: number;
   totalBytes: number;
 
@@ -36,7 +42,7 @@ export class PdfVisualizationComponent implements OnChanges, OnDestroy {
   public readonly minZoom: number = 0.1;
   public readonly maxZoom: number = 4.0;
 
-  constructor(private sessionDataService: SessionDataService, private restErrorService: RestErrorService) {}
+  constructor(private sessionDataService: SessionDataService, private restErrorService: RestErrorService) { }
 
   ngOnChanges() {
     // unsubscribe from previous subscriptions
@@ -84,8 +90,45 @@ export class PdfVisualizationComponent implements OnChanges, OnDestroy {
   }
 
   pdfLoadComplete(pdf: any) {
+
     this.totalPages = pdf.numPages;
     this.state = new LoadState(State.Ready);
+
+    // store page heights      
+    for (let i = 1; i <= this.totalPages; i++) {
+      pdf.getPage(i).then(page => {
+        // 0: x, 1: y, 2: width, 3: height
+        this.unscaledPageHeights[i] = page.view[3];
+      })
+    }
+  }
+
+  pageRendered(event: any) {
+    let viewport = event.source.viewport;
+
+    // calcualte the total height of all visible pages
+    if (this.showAll) {
+      let totalHeight = 0;
+
+      // sum of all pages
+      for (let i = 1; i <= this.totalPages; i++) {
+        if (i < this.unscaledPageHeights.length) {
+
+          // we could get the scaled height of page directly from viewport.height, but only after the page is rendered
+          // viewport.scale is different than this.zoom, maybe calcualated from page width and the container width?
+          // <div class="page"> has 10px bottom margin
+          totalHeight += this.unscaledPageHeights[i] * viewport.scale + 10;
+        } else {
+          console.log("no height for page " + i);
+        }
+      }
+
+      this.height = totalHeight;
+
+    } else {
+      // height of the current page, see more comments above
+      this.height = this.unscaledPageHeights[this.page] * viewport.scale + 10;
+    }
   }
 
   onProgress(progressData: any) {
